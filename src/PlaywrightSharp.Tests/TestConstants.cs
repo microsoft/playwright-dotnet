5ad1a85b7677d783b6cffaa081b02fb02f7fa30f
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-01-27 18:29:53 -0300

    Remove unneeded references

--- src/PlaywrightSharp.Tests/TestConstants.cs
+++ src/PlaywrightSharp.Tests/TestConstants.cs
@@ -17,7 +17,7 @@ namespace PlaywrightSharp.Tests
         public const string ServerIpUrl = "http://127.0.0.1:8081";
         public const string HttpsPrefix = "https://localhost:8082";
         public const string AboutBlank = "about:blank";
-        public static readonly string CrossProcessHttpPrefix = "http://127.0.0.1:8081";
+        public const string CrossProcessHttpPrefix = "http://127.0.0.1:8081";
         public static readonly string EmptyPage = $"{ServerUrl}/empty.html";
         public static readonly string CrossProcessUrl = ServerIpUrl;
         public static readonly string ExtensionPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "simple-extension");
