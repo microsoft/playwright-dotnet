8035d7fa607a965b2a179f4c66794aec14a55ec8
Meir Blachman <meirblachman@gmail.com>
2020-01-31 15:09:27 +0200

    skeleton

---
+++ src/PlaywrightSharp.Tests/Mouse/MouseTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using PlaywrightSharp.Tests.BaseTests;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.Mouse
+{
+    ///<playwright-file>mouse.spec.js</playwright-file>
+    ///<playwright-describe>Mouse</playwright-describe>
+    public class MouseTests : PlaywrightSharpPageBaseTest
+    {
+        internal MouseTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should click the document</playwright-it>
+        public async Task ShouldClickTheDocument()
+        {
+
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should select the text with mouse</playwright-it>
+        public async Task ShouldSelectTheTextWithMouse()
+        {
+
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should trigger hover state</playwright-it>
+        public async Task ShouldTriggerHoverState()
+        {
+
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should trigger hover state with removed window.Node</playwright-it>
+        public async Task ShouldTriggerHoverStateWithRemovedWindow.Node()
+        {
+
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should set modifier keys on click</playwright-it>
+        public async Task ShouldSetModifierKeysOnClick()
+        {
+
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should tween mouse movement</playwright-it>
+        public async Task ShouldTweenMouseMovement()
+        {
+
+        }
+
+        ///<playwright-file>mouse.spec.js</playwright-file>
+        ///<playwright-describe>Mouse</playwright-describe>
+        ///<playwright-it>should work with mobile viewports and cross process navigations</playwright-it>
+        public async Task ShouldWorkWithMobileViewportsAndCrossProcessNavigations()
+        {
+
+        }
+    }
+}
