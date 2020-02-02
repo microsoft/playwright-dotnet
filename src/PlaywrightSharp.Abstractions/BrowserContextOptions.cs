ac592debffabe03dfcea002e975e76dadf6a3cfd
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-02-02 10:08:38 -0300

    emulate tests (#172)
    * emulate tests
    
    * Code factor

--- src/PlaywrightSharp.Abstractions/BrowserContextOptions.cs
+++ src/PlaywrightSharp.Abstractions/BrowserContextOptions.cs
@@ -21,5 +21,10 @@
         /// Whether or not to enable or disable JavaScript in the context. Defaults to true.
         /// </summary>
         public bool JavaScriptEnabled { get; set; }
+
+        /// <summary>
+        /// Changes the timezone of the context. See <see href="https://cs.chromium.org/chromium/src/third_party/icu/source/data/misc/metaZones.txt?rcl=faee8bc70570192d82d2978a71e2a615788597d1">ICU’s metaZones.txt</see> for a list of supported timezone IDs.
+        /// </summary>
+        public string TimezoneId { get; set; }
     }
 }
---
+++ src/PlaywrightSharp.Abstractions/PageEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace PlaywrightSharp
+{
+    /// <summary>
+    /// Enums for <see cref="IPage.WaitForEvent(PageEvent)"/>
+    /// </summary>
+    public enum PageEvent
+    {
+        /// <summary>
+        /// Console event
+        /// </summary>
+        Console
+    }
+}
