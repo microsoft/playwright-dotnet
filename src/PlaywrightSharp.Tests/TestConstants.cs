b0fac22e787c13485a24a848c427e541142897a8
Meir Blachman <meirblachman@gmail.com>
2020-02-02 15:02:36 +0200

    Mouse tests (#168)
    * skeleton
    
    * added tests
    
    * docs
    
    * deleted extra variable
    
    * formatting
    
    * Update src/PlaywrightSharp.Tests/TestConstants.cs
    
    Co-Authored-By: Darío Kondratiuk <dariokondratiuk@gmail.com>
    
    Co-authored-by: Darío Kondratiuk <dariokondratiuk@gmail.com>

--- src/PlaywrightSharp.Tests/TestConstants.cs
+++ src/PlaywrightSharp.Tests/TestConstants.cs
@@ -29,6 +29,7 @@ namespace PlaywrightSharp.Tests
         internal static bool IsWebKit = false;
         internal static bool IsFirefox = false;
         internal static bool IsChromium = true;
+        internal static bool IsMacOSX = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
 
         public static readonly IEnumerable<string> NestedFramesDumpResult = new List<string>()
         {
