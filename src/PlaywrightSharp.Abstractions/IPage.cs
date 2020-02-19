e5b42ba43d1bfd27254ef9a36f211b0372b77f14
Meir Blachman <meirblachman@gmail.com>
2020-02-19 07:08:45 +0200

    cr rename

--- src/PlaywrightSharp.Abstractions/IPage.cs
+++ src/PlaywrightSharp.Abstractions/IPage.cs
@@ -500,7 +500,7 @@ namespace PlaywrightSharp
         /// <param name="text"><![CDATA[Value to fill for the <input>, <textarea> or [contenteditable] element]]></param>
         /// <param name="options">Optional waiting parameters.</param>
         /// <returns>A <see cref="Task"/> that completes when the fill message is confirmed by the browser.</returns>
-        Task FillAsync(string selector, string text, WaitForFillOptions options = null);
+        Task FillAsync(string selector, string text, FillOptions options = null);
 
         /// <summary>
         /// Sends a <c>keydown</c>, <c>keypress</c>/<c>input</c>, and <c>keyup</c> event for each character in the text.
---
+++ src/PlaywrightSharp.Abstractions/WaitForOption.cs
@@ -0,0 +1,30 @@
+namespace PlaywrightSharp
+{
+    /// <summary>
+    /// Wair for options for <see cref="ClickOptions.WaitFor"/>.
+    /// </summary>
+    /// <seealso cref="ClickOptions.WaitFor"/>
+    /// <seealso cref="FillOptions.WaitFor"/>
+    public enum WaitForOption
+    {
+        /// <summary>
+        /// Wait for visible
+        /// </summary>
+        Visible,
+
+        /// <summary>
+        /// Wait for hidden
+        /// </summary>
+        Hidden,
+
+        /// <summary>
+        /// Wait for any
+        /// </summary>
+        Any,
+
+        /// <summary>
+        /// No wait
+        /// </summary>
+        NoWait,
+    }
+}
