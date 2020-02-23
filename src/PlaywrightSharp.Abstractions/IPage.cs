b4c09085c21b0e3b9aa69173f26b4efeda67131e
Meir Blachman <meirblachman@gmail.com>
2020-02-23 14:06:45 +0200

    add query selector tests (#234)
    * add query selector tests
    
    * almost done
    
    * code factor
    
    * code factor
    
    * formatting

--- src/PlaywrightSharp.Abstractions/IPage.cs
+++ src/PlaywrightSharp.Abstractions/IPage.cs
@@ -453,7 +453,7 @@ namespace PlaywrightSharp
         Task EvaluateOnNewDocumentAsync(string pageFunction, params object[] args);
 
         /// <summary>
-        /// This method runs document.querySelector within the page and passes it as the first argument to pageFunction.
+        /// This method runs <c>document.querySelector</c> within the page and passes it as the first argument to pageFunction.
         /// If there's no element matching selector, the method throws an error.
         /// </summary>
         /// <param name="selector">A selector to query page for.</param>
@@ -466,7 +466,7 @@ namespace PlaywrightSharp
         Task QuerySelectorEvaluateAsync(string selector, string script, params object[] args);
 
         /// <summary>
-        /// This method runs document.querySelector within the page and passes it as the first argument to pageFunction.
+        /// This method runs <c>document.querySelector</c> within the page and passes it as the first argument to pageFunction.
         /// If there's no element matching selector, the method throws an error.
         /// </summary>
         /// <typeparam name="T">Result type.</typeparam>
@@ -478,6 +478,31 @@ namespace PlaywrightSharp
         /// </remarks>
         /// <returns>A <see cref="Task"/> that completes when the script finishes or the promise is resolved, yielding the result of the script.</returns>
         Task<T> QuerySelectorEvaluateAsync<T>(string selector, string script, params object[] args);
+
+        /// <summary>
+        /// This method runs <c>Array.from(document.querySelectorAll(selector))</c> within the page and passes it as the first argument to pageFunction.
+        /// </summary>
+        /// <param name="selector">A selector to query page for.</param>
+        /// <param name="script">Script to be evaluated in browser context.</param>
+        /// <param name="args">Arguments to pass to script.</param>
+        /// <remarks>
+        /// If the script, returns a Promise, then the method would wait for the promise to resolve and return its value.
+        /// </remarks>
+        /// <returns>A <see cref="Task"/> that completes when the script finishes or the promise is resolved, yielding the result of the script.</returns>
+        Task QuerySelectorAllEvaluateAsync(string selector, string script, params object[] args);
+
+        /// <summary>
+        /// This method runs <c>Array.from(document.querySelectorAll(selector))</c> within the page and passes it as the first argument to pageFunction.
+        /// </summary>
+        /// <typeparam name="T">Result type.</typeparam>
+        /// <param name="selector">A selector to query page for.</param>
+        /// <param name="script">Script to be evaluated in browser context.</param>
+        /// <param name="args">Arguments to pass to script.</param>
+        /// <remarks>
+        /// If the script, returns a Promise, then the method would wait for the promise to resolve and return its value.
+        /// </remarks>
+        /// <returns>A <see cref="Task"/> that completes when the script finishes or the promise is resolved, yielding the result of the script.</returns>
+        Task<T> QuerySelectorAllEvaluateAsync<T>(string selector, string script, params object[] args);
 
         /// <summary>
         /// <![CDATA[
@@ -629,9 +654,18 @@ namespace PlaywrightSharp
         /// </summary>
         /// <param name="selector">A selector to query page for.</param>
         /// <returns>
-        /// A <see cref="Task"/> that completes when the javascription function finishes, yielding an <see cref="IElementHandle"/> pointing to the frame element.
+        /// A <see cref="Task"/> that completes when the javascription function finishes, yielding an <see cref="IElementHandle"/>.
         /// </returns>
         Task<IElementHandle> QuerySelectorAsync(string selector);
+
+        /// <summary>
+        /// The method runs <c>Array.from(document.querySelectorAll(selector))</c> within the page.
+        /// </summary>
+        /// <param name="selector">A selector to query page for.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that completes when the javascription function finishes, yielding an array of <see cref="IElementHandle"/>.
+        /// </returns>
+        Task<IElementHandle[]> QuerySelectorAllAsync(string selector);
 
         /// <summary>
         /// Executes a script in browser context.
---
+++ src/PlaywrightSharp.Tests/QuerySelector/SelectorsRegisterTests.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Threading.Tasks;
+using PlaywrightSharp.Tests.BaseTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.QuerySelector
+{
+    ///<playwright-file>queryselector.spec.js</playwright-file>
+    ///<playwright-describe>selectors.register</playwright-describe>
+    public class SelectorsRegisterTests : PlaywrightSharpPageBaseTest
+    {
+        internal SelectorsRegisterTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        internal ISelectors Selectors { get; set; }
+
+        ///<playwright-file>queryselector.spec.js</playwright-file>
+        ///<playwright-describe>selectors.register</playwright-describe>
+        ///<playwright-it>should work</playwright-it>
+        [Fact]
+        public async Task ShouldWork()
+        {
+            string createTagSelector = @"() => ({
+                name: 'tag',
+                create(root, target) {
+                    return target.nodeName;
+                },
+                query(root, selector) {
+                    return root.querySelector(selector);
+                },
+                queryAll(root, selector) {
+                    return Array.from(root.querySelectorAll(selector));
+                }
+            })";
+            await Selectors.RegisterAsync($"({createTagSelector})()");
+            await Page.SetContentAsync("<div><span></span></div><div></div>");
+            Assert.Equal("DIV", await Selectors.CreateSelectorAsync("tag", await Page.QuerySelectorAsync("div")));
+            Assert.Equal("DIV", await Page.QuerySelectorEvaluateAsync<string>("tag=DIV", "e => e.nodeName"));
+            Assert.Equal("SPAN", await Page.QuerySelectorEvaluateAsync<string>("tag=SPAN", "e => e.nodeName"));
+            Assert.Equal(2, await Page.QuerySelectorAllEvaluateAsync<int>("tag=DIV", "es => es.length"));
+        }
+
+        ///<playwright-file>queryselector.spec.js</playwright-file>
+        ///<playwright-describe>selectors.register</playwright-describe>
+        ///<playwright-it>should update</playwright-it>
+        [Fact]
+        public async Task ShouldUpdate()
+        {
+            await Page.SetContentAsync("<div><dummy id=d1></dummy></div><span><dummy id=d2></dummy></span>");
+            Assert.Equal("DIV", await Page.QuerySelectorEvaluateAsync<string>("div", "e => e.nodeName"));
+            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => Page.QuerySelectorAsync("dummy=foo"));
+            Assert.Contains("Unknown engine dummy while parsing selector dummy=foo", exception.Message);
+            string createDummySelector = @"(name) => ({
+                name,
+                create(root, target) {
+                    return target.nodeName;
+                },
+                query(root, selector) {
+                    return root.querySelector(name);
+                },
+                queryAll(root, selector) {
+                    return Array.from(root.querySelectorAll(name));
+                }
+            })";
+            await Selectors.RegisterAsync(createDummySelector, "dummy");
+            Assert.Equal("d1", await Page.QuerySelectorEvaluateAsync<string>("dummy=foo", "e => e.id"));
+            Assert.Equal("d2", await Page.QuerySelectorEvaluateAsync<string>("css=span >> dummy=foo", "e => e.id"));
+        }
+    }
+}
