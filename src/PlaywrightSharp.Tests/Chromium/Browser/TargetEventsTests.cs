06330c302776504b36d98b6905d27d096de89b0b
Meir Blachman <meirblachman@gmail.com>
2020-03-01 07:29:03 +0200

    skip wip test

--- src/PlaywrightSharp.Tests/Chromium/Browser/TargetEventsTests.cs
+++ src/PlaywrightSharp.Tests/Chromium/Browser/TargetEventsTests.cs
@@ -23,7 +23,7 @@ namespace PlaywrightSharp.Tests.Chromium.Browser
         ///<playwright-file>chromium/launcher.spec.js</playwright-file>
         ///<playwright-describe>Browser target events</playwright-describe>
         ///<playwright-it>should work</playwright-it>
-        [SkipBrowserAndPlatformFact(skipFirefox: true, skipWebkit: true, skipChromium: true)]
+        [SkipBrowserAndPlatformFact(skipFirefox: true, skipWebkit: true)]
         public async Task ShouldWork()
         {
             using var browser = await Playwright.LaunchAsync(TestConstants.DefaultBrowserOptions);
