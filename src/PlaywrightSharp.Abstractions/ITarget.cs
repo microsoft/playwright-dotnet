5b4e05b278588cba650933ae0a0194c84bbd6a66
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-03-19 07:44:39 -0300

    Chromium headful tests (#258)
    * Chromium headful tests
    
    * Dot not run them yet

--- src/PlaywrightSharp.Abstractions/ITarget.cs
+++ src/PlaywrightSharp.Abstractions/ITarget.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace PlaywrightSharp
 {
     /// <summary>
@@ -14,5 +16,14 @@ namespace PlaywrightSharp
         /// Gets the type. It will be <see cref="ITarget.Type"/>.
         /// </summary>
         TargetType Type { get; }
+
+        /// <summary>
+        /// Terget's <see cref="IPage"/>.
+        /// </summary>
+        /// <remarks>
+        /// If the target is not of type <see cref="TargetType.Page"/> or <see cref="TargetType.BackgroundPage"/>, returns null.
+        /// </remarks>
+        /// <returns>A <see cref="Task"/> that completes when the corresponding <see cref="IPage"/> is found, yielding its <see cref="IPage"/>.</returns>
+        Task<IPage> GetPageAsync();
     }
 }
--- src/PlaywrightSharp.Chromium/ChromiumTarget.cs
+++ src/PlaywrightSharp.Chromium/ChromiumTarget.cs
@@ -108,6 +108,12 @@ namespace PlaywrightSharp.Chromium
             return _workerTask;
         }
 
+        /// <inheritdoc cref="ITarget.GetPageAsync"/>
+        public Task<IPage> GetPageAsync()
+        {
+            throw new NotImplementedException();
+        }
+
         internal async Task<IPage> PageAsync()
         {
             if ((Type == TargetType.Page || Type == TargetType.BackgroundPage) && PageTask == null)
