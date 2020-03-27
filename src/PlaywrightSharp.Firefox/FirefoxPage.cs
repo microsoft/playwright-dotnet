54829396c417b0852570fa345606d9f5963f0ddd
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-03-27 08:11:07 -0300

    ops

--- src/PlaywrightSharp.Firefox/FirefoxPage.cs
+++ src/PlaywrightSharp.Firefox/FirefoxPage.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Text.Json;
 using System.Threading.Tasks;
 using PlaywrightSharp.Firefox.Helper;
 using PlaywrightSharp.Firefox.Input;
