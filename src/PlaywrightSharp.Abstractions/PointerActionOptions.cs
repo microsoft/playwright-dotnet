c4a940e4661bb0053588dccebc4059827e8707e9
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-04-02 08:01:50 -0300

    Chromium: Pass ElementHandleHoverTests (#287)

---
+++ src/PlaywrightSharp.Abstractions/PointerActionOptions.cs
@@ -0,0 +1,22 @@
+using System.Drawing;
+using PlaywrightSharp.Input;
+
+namespace PlaywrightSharp
+{
+    /// <summary>
+    /// Pointer options.
+    /// </summary>
+    /// <seealso cref="IElementHandle.HoverAsync"/>
+    public class PointerActionOptions : IPointerActionOptions
+    {
+        /// <summary>
+        /// A point to click relative to the top-left corner of element padding box. If not specified, clicks to some visible point of the element.
+        /// </summary>
+        public Point RelativePoint { get; set; }
+
+        /// <summary>
+        /// Modifier keys to press. Ensures that only these modifiers are pressed during the click, and then restores current modifiers back. If not specified, currently pressed modifiers are used.
+        /// </summary>
+        public Modifier[] Modifiers { get; set; }
+    }
+}
