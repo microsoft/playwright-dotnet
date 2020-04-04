4c2aa381e13b47957f4ed1c40fca6cf2b5169979
Meir Blachman <meirblachman@gmail.com>
2020-04-04 20:53:26 +0300

    for ci

--- src/PlaywrightSharp.Tests/Page/PageCloseTests.cs
+++ src/PlaywrightSharp.Tests/Page/PageCloseTests.cs
@@ -53,7 +53,7 @@ namespace PlaywrightSharp.Tests.Page
             // We have to interact with a page so that 'beforeunload' handlers
             // fire.
             await newPage.ClickAsync("body");
-            var pageClosingPromise = newPage.CloseAsync(new PageCloseOptions { RunBeforeUnload = true });
+            var pageClosingTask = newPage.CloseAsync(new PageCloseOptions { RunBeforeUnload = true });
             var dialog = await newPage.WaitForEvent<DialogEventArgs>(PageEvent.Dialog).ContinueWith(task => task.Result.Dialog);
             Assert.Equal(DialogType.BeforeUnload, dialog.DialogType);
             Assert.Empty(dialog.DefaultValue);
@@ -71,7 +71,7 @@ namespace PlaywrightSharp.Tests.Page
             }
 
             await dialog.AcceptAsync();
-            await pageClosingPromise;
+            await pageClosingTask;
         }
 
         ///<playwright-file>page.spec.js</playwright-file>
