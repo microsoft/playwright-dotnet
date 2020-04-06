605839261efd36a739973685ece63800f234ffe7
Meir Blachman <meirblachman@gmail.com>
2020-04-06 16:47:22 +0300

    cleanup

--- src/PlaywrightSharp.Firefox/Input/FirefoxRawMouse.cs
+++ src/PlaywrightSharp.Firefox/Input/FirefoxRawMouse.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using PlaywrightSharp.Firefox.Helper;
 using PlaywrightSharp.Firefox.Protocol.Page;
 using PlaywrightSharp.Input;
 
@@ -10,20 +10,17 @@ namespace PlaywrightSharp.Firefox.Input
     {
         private readonly FirefoxSession _session;
 
-        public FirefoxRawMouse(FirefoxSession session)
-        {
-            _session = session;
-        }
+        public FirefoxRawMouse(FirefoxSession session) => _session = session;
 
         public Task DownAsync(double x, double y, MouseButton lastButton, List<MouseButton> buttons, Modifier[] modifiers, int clickCount)
             => _session.SendAsync(new PageDispatchMouseEventRequest
             {
                 Type = "mousedown",
-                Button = ToButtonNumber(lastButton),
-                Buttons = ToButtonsMask(buttons),
+                Button = lastButton.ToButtonNumber(),
+                Buttons = buttons.ToButtonsMask(),
                 X = x,
                 Y = y,
-                Modifiers = ToModifiersMask(modifiers),
+                Modifiers = modifiers.ToModifiersMask(),
                 ClickCount = clickCount,
             });
 
@@ -32,77 +29,22 @@ namespace PlaywrightSharp.Firefox.Input
             {
                 Type = "mousemove",
                 Button = 0,
-                Buttons = ToButtonsMask(buttons),
+                Buttons = buttons.ToButtonsMask(),
                 X = x,
                 Y = y,
-                Modifiers = ToModifiersMask(modifiers),
+                Modifiers = modifiers.ToModifiersMask(),
             });
 
         public Task UpAsync(double x, double y, MouseButton lastButton, List<MouseButton> buttons, Modifier[] modifiers, int clickCount)
             => _session.SendAsync(new PageDispatchMouseEventRequest
             {
                 Type = "mouseup",
-                Button = ToButtonNumber(lastButton),
-                Buttons = ToButtonsMask(buttons),
+                Button = lastButton.ToButtonNumber(),
+                Buttons = buttons.ToButtonsMask(),
                 X = x,
                 Y = y,
-                Modifiers = ToModifiersMask(modifiers),
+                Modifiers = modifiers.ToModifiersMask(),
                 ClickCount = clickCount,
             });
-
-        private static int ToModifiersMask(Modifier[] modifiers)
-        {
-            int mask = 0;
-            if (modifiers.Contains(Modifier.Alt))
-            {
-                mask |= 1;
-            }
-
-            if (modifiers.Contains(Modifier.Control))
-            {
-                mask |= 2;
-            }
-
-            if (modifiers.Contains(Modifier.Shift))
-            {
-                mask |= 4;
-            }
-
-            if (modifiers.Contains(Modifier.Meta))
-            {
-                mask |= 8;
-            }
-
-            return mask;
-        }
-
-        private static int ToButtonNumber(MouseButton button) => button switch
-        {
-            MouseButton.Left => 0,
-            MouseButton.Middle => 1,
-            MouseButton.Right => 2,
-            _ => 0,
-        };
-
-        private static int ToButtonsMask(List<MouseButton> buttons)
-        {
-            int mask = 0;
-            if (buttons.Contains(MouseButton.Left))
-            {
-                mask |= 1;
-            }
-
-            if (buttons.Contains(MouseButton.Right))
-            {
-                mask |= 2;
-            }
-
-            if (buttons.Contains(MouseButton.Middle))
-            {
-                mask |= 4;
-            }
-
-            return mask;
-        }
     }
 }
