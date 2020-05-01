758fa2d2d988daae45e499b60a19c98e808ffdc0
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-05-01 08:12:52 -0300

    Chromium: Pass WebSocketTests
    relates to #71

--- src/PlaywrightSharp.Tests/Page/WebSocketTests.cs
+++ src/PlaywrightSharp.Tests/Page/WebSocketTests.cs
@@ -1,14 +1,19 @@
 using PlaywrightSharp.Tests.BaseTests;
+using Xunit;
 using Xunit.Abstractions;
 
-namespace PlaywrightSharp.Tests.Page.Network
+namespace PlaywrightSharp.Tests.Page
 {
     // All these tests are bypassed on Playwright
     ///<playwright-file>network.spec.js</playwright-file>
     ///<playwright-describe>WebSocket</playwright-describe>
+    [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
+    [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class WebSocketTests : PlaywrightSharpPageBaseTest
     {
-        internal WebSocketTests(ITestOutputHelper output) : base(output)
+        /// <inheritdoc/>
+        public WebSocketTests(ITestOutputHelper output) : base(output)
         {
         }
     }
