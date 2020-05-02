3b5dd80b88610d7445a6f2bd00fc96f570d51614
Meir Blachman <meirblachman@gmail.com>
2020-05-02 21:20:19 +0300

    pass ResponseBufferTests

--- src/PlaywrightSharp.Tests/Page/Network/ResponseBufferTests.cs
+++ src/PlaywrightSharp.Tests/Page/Network/ResponseBufferTests.cs
@@ -9,6 +9,7 @@ namespace PlaywrightSharp.Tests.Page.Network
     ///<playwright-file>network.spec.js</playwright-file>
     ///<playwright-describe>Response.buffer</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class ResponseBufferTests : PlaywrightSharpPageBaseTest
     {
