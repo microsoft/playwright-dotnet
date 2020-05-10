0a5f877c1ce33948f64ca40018088c7df7498030
Meir Blachman <meirblachman@gmail.com>
2020-05-10 18:18:46 +0300

    firefox: pass DefaultBrowserContextTests

--- src/PlaywrightSharp.Tests/BrowserContext/DefaultBrowserContextTests.cs
+++ src/PlaywrightSharp.Tests/BrowserContext/DefaultBrowserContextTests.cs
@@ -9,6 +9,7 @@ namespace PlaywrightSharp.Tests.BrowserContext
     /// <playwright-file>defaultbrowsercontext.spec.js</playwright-file>
     /// <playwright-describe>defaultContext()</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class DefaultBrowserContextTests : PlaywrightSharpPageBaseTest
     {
