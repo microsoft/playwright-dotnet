8000892624623ace21b85ab27028479c7bf2832a
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-05-15 08:48:51 -0300

    Add categories

--- src/PlaywrightSharp.Tests/Chromium/Launcher/WebSocketOptionsTests.cs
+++ src/PlaywrightSharp.Tests/Chromium/Launcher/WebSocketOptionsTests.cs
@@ -9,6 +9,8 @@ namespace PlaywrightSharp.Tests.Chromium.Launcher
     ///<playwright-file>chromium/launcher.spec.js</playwright-file>
     ///<playwright-describe>Playwright.launch webSocket option</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
+    [Trait("Category", "webkit")]
     [Collection(TestConstants.TestFixtureCollectionName)]
     public class WebSocketOptionsTests : PlaywrightSharpBaseTest
     {
