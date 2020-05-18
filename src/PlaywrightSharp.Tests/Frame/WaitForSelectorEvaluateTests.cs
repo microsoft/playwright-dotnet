bf81765abf1e5bab9430326a09a818eca6a3a6f8
Meir Blachman <meirblachman@gmail.com>
2020-05-18 14:22:43 +0300

    Firefox: Pass WaitForSelectorEvaluateTests (#395)

--- src/PlaywrightSharp.Tests/Frame/WaitForSelectorEvaluateTests.cs
+++ src/PlaywrightSharp.Tests/Frame/WaitForSelectorEvaluateTests.cs
@@ -8,6 +8,7 @@ namespace PlaywrightSharp.Tests.Frame
     ///<playwright-file>waittask.spec.js</playwright-file>
     ///<playwright-describe>Frame.$wait</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class WaitForSelectorEvaluateTests : PlaywrightSharpPageBaseTest
     {
