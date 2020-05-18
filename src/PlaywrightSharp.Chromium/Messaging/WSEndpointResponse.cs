1c53b1a34f2860bfcc4d64f4eab5a8ab6f1c8861
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-05-18 08:20:43 -0300

    Chromium: Pass BrowserUrlOptionTests (#391)

--- src/PlaywrightSharp.Chromium/Messaging/WSEndpointResponse.cs
+++ src/PlaywrightSharp.Chromium/Messaging/WSEndpointResponse.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace PlaywrightSharp.Chromium.Messaging
 {
     internal class WSEndpointResponse
     {
+        [JsonPropertyName("webSocketDebuggerUrl")]
         public string WebSocketDebuggerUrl { get; set; }
     }
 }
