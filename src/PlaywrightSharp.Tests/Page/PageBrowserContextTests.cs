1a406d615db86590e5b0b0e5fe811fe05d1e8a6e
Meir Blachman <meirblachman@gmail.com>
2020-06-01 17:34:26 +0300

    Firefox: pass PageBrowserContextTests

--- src/PlaywrightSharp.Tests/Page/PageBrowserContextTests.cs
+++ src/PlaywrightSharp.Tests/Page/PageBrowserContextTests.cs
@@ -8,6 +8,7 @@ namespace PlaywrightSharp.Tests.Page
     ///<playwright-file>page.spec.js</playwright-file>
     ///<playwright-describe>Page.browserContext</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class PageBrowserContextTests : PlaywrightSharpPageBaseTest
     {
