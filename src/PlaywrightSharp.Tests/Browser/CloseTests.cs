6a482029f78bf857bc9af2eb809107dab3914c91
Meir Blachman <meirblachman@gmail.com>
2020-06-08 14:33:24 +0300

    skip CloseTests

--- src/PlaywrightSharp.Tests/Browser/CloseTests.cs
+++ src/PlaywrightSharp.Tests/Browser/CloseTests.cs
@@ -9,7 +9,6 @@ namespace PlaywrightSharp.Tests.Browser
     ///<playwright-file>launcher.spec.js</playwright-file>
     ///<playwright-describe>Browser.close</playwright-describe>
     [Trait("Category", "chromium")]
-    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureCollectionName)]
     public class CloseTests : PlaywrightSharpBaseTest
     {
