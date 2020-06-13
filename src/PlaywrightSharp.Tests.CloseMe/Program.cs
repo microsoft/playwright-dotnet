f9bebc0d1c525ad5c3d4a93ff8b7a960e77c7455
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-06-13 16:53:32 -0300

    Feature complete

--- src/PlaywrightSharp.Tests.CloseMe/Program.cs
+++ src/PlaywrightSharp.Tests.CloseMe/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using PlaywrightSharp.Chromium;
+using PlaywrightSharp.Firefox;
 
 namespace PlaywrightSharp.Tests.CloseMe
 {
     class Program
     {
-        static void Main(string[] args)
+        public const string ChromiumProduct = "CHROMIUM";
+        public const string WebkitProduct = "WEBKIT";
+        public const string FirefoxProduct = "FIREFOX";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = new LaunchOptions
+            {
+                ExecutablePath = args[0],
+            };
+
+            IBrowserType playwright = args[1] switch
+            {
+                WebkitProduct => null,
+                FirefoxProduct => new FirefoxBrowserType(),
+                ChromiumProduct => new ChromiumBrowserType(),
+                _ => throw new ArgumentOutOfRangeException($"product {args[1]} does not exist")
+            };
+
+            var browserApp = await playwright.LaunchBrowserAppAsync(options);
+
+            browserApp.Closed += (sender, eventArgs) => Console.WriteLine($"browserClose:{browserApp.Process.ExitCode}:browserClose");
+
+            Console.WriteLine($"browserPid:{browserApp.Process.Id}:browserPid");
+            Console.WriteLine($"browserWS:{browserApp.WebSocketEndpoint}:browserWS");
+            Console.ReadLine();
         }
     }
 }
