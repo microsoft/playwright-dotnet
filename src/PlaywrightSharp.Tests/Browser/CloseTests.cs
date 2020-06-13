560133dba37a0bcbd006650a30beaed0d14f54fe
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-06-13 17:49:16 -0300

    Some progress
    # Conflicts:
    #	src/PlaywrightSharp.Firefox/FirefoxConnection.cs

--- src/PlaywrightSharp.Tests/Browser/CloseTests.cs
+++ src/PlaywrightSharp.Tests/Browser/CloseTests.cs
@@ -9,6 +9,7 @@ namespace PlaywrightSharp.Tests.Browser
     ///<playwright-file>launcher.spec.js</playwright-file>
     ///<playwright-describe>Browser.close</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureCollectionName)]
     public class CloseTests : PlaywrightSharpBaseTest
     {
