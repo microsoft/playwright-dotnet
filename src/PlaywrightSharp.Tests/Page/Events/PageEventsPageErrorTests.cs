bdc523c9c896c784f10765a91586c99a03334cc3
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-06-15 09:52:43 -0300

    Firefox: Pass PageEventsPageErrorTests (#613)

--- src/PlaywrightSharp.Tests/Page/Events/PageEventsPageErrorTests.cs
+++ src/PlaywrightSharp.Tests/Page/Events/PageEventsPageErrorTests.cs
@@ -9,6 +9,7 @@ namespace PlaywrightSharp.Tests.Page
     ///<playwright-file>page.spec.js</playwright-file>
     ///<playwright-describe>Page.Events.PageError</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class PageEventsPageErrorTests : PlaywrightSharpPageBaseTest
     {
