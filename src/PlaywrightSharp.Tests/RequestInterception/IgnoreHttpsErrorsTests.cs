3624ca7c154e5d7d9a124c9bdcb044f745e6d599
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-06-17 09:23:18 -0300

    Firefox: Pass IgnoreHttpsErrorsTests
    related to #106

--- src/PlaywrightSharp.Tests/RequestInterception/IgnoreHttpsErrorsTests.cs
+++ src/PlaywrightSharp.Tests/RequestInterception/IgnoreHttpsErrorsTests.cs
@@ -10,6 +10,7 @@ namespace PlaywrightSharp.Tests.RequestInterception
     ///<playwright-file>interception.spec.js</playwright-file>
     ///<playwright-describe>ignoreHTTPSErrors</playwright-describe>
     [Trait("Category", "chromium")]
+    [Trait("Category", "firefox")]
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
     public class IgnoreHttpsErrorsTests : PlaywrightSharpPageBaseTest
     {
