c54793243c6617ce320e5af1967ea3a7bdd2260b
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-06-19 09:03:31 -0300

    Firefox: Screenshot Demo (#629)

---
+++ demos/ScreenshotsDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using PlaywrightSharp;
+using PlaywrightSharp.Firefox;
+
+namespace PdfDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new LaunchOptions
+            {
+                Headless = true
+            };
+
+            Console.WriteLine("Downloading Firefox");
+            var firefox = new FirefoxBrowserType();
+
+            await firefox.CreateBrowserFetcher().DownloadAsync();
+
+            Console.WriteLine("Navigating google");
+            await using var browser = await firefox.LaunchAsync(options);
+            var page = await browser.DefaultContext.NewPageAsync();
+            await page.GoToAsync("http://www.microsoft.com");
+
+            Console.WriteLine("Taking Screenshot");
+            File.WriteAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "microsoft.png"), await page.ScreenshotAsync());
+
+            Console.WriteLine("Export completed");
+        }
+    }
+}
