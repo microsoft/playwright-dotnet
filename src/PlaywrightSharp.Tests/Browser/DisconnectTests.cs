8551792cb6514b3b800cbd60a4e6fd27b8cb19fe
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-07-29 20:36:29 -0300

    Fix test

--- src/PlaywrightSharp.Tests/Browser/DisconnectTests.cs
+++ src/PlaywrightSharp.Tests/Browser/DisconnectTests.cs
@@ -22,7 +22,7 @@ namespace PlaywrightSharp.Tests.Browser
         [Retry]
         public async Task ShouldRejectNavigationWhenBrowserCloses()
         {
-            Server.SetRoute("/one-style.css", context => Task.Delay(10000));
+            Server.SetRoute("/one-style.css", context => Task.CompletedTask);
 
             await using var browserServer = await BrowserType.LaunchServerAsync(TestConstants.GetDefaultBrowserOptions());
 
