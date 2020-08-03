021382dffe2337f8d482a78c5fe458fa01aa7e64
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-08-03 18:38:32 -0300

    Pass ElementHandleClickTests (#677)
    related to #498

--- src/PlaywrightSharp.Tests/ElementHandle/ElementHandleClickTests.cs
+++ src/PlaywrightSharp.Tests/ElementHandle/ElementHandleClickTests.cs
@@ -9,8 +9,7 @@ namespace PlaywrightSharp.Tests.ElementHandle
     ///<playwright-file>elementhandle.spec.js</playwright-file>
     ///<playwright-describe>ElementHandle.click</playwright-describe>
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1000:Test classes must be public", Justification = "Disabled")]
-    class ElementHandleClickTests : PlaywrightSharpPageBaseTest
+    public class ElementHandleClickTests : PlaywrightSharpPageBaseTest
     {
         /// <inheritdoc/>
         public ElementHandleClickTests(ITestOutputHelper output) : base(output)
@@ -62,8 +61,8 @@ namespace PlaywrightSharp.Tests.ElementHandle
         {
             await Page.GoToAsync(TestConstants.ServerUrl + "/input/button.html");
             var buttonTextNode = (IElementHandle)await Page.EvaluateHandleAsync("() => document.querySelector('button').firstChild");
-            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => buttonTextNode.ClickAsync());
-            Assert.Equal("Node is not of type HTMLElement", exception.Message);
+            await buttonTextNode.ClickAsync();
+            Assert.Equal("Clicked", await Page.EvaluateAsync<string>("() => result"));
         }
 
         ///<playwright-file>elementhandle.spec.js</playwright-file>
@@ -76,45 +75,67 @@ namespace PlaywrightSharp.Tests.ElementHandle
             var button = await Page.QuerySelectorAsync("button");
             await Page.EvaluateAsync("button => button.remove()", button);
             var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => button.ClickAsync());
-            Assert.Equal("Node is detached from document", exception.Message);
+            Assert.Contains("Element is not attached to the DOM", exception.Message);
         }
 
         ///<playwright-file>elementhandle.spec.js</playwright-file>
         ///<playwright-describe>ElementHandle.click</playwright-describe>
-        ///<playwright-it>should throw for hidden nodes</playwright-it>
+        ///<playwright-it>should throw for hidden nodes with force</playwright-it>
         [Retry]
-        public async Task ShouldThrowForHiddenNodes()
+        public async Task ShouldThrowForHiddenNodesWithForce()
         {
             await Page.GoToAsync(TestConstants.ServerUrl + "/input/button.html");
             var button = await Page.QuerySelectorAsync("button");
             await Page.EvaluateAsync("button => button.style.display = 'none'", button);
-            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => button.ClickAsync());
-            Assert.Equal("Node is either not visible or not an HTMLElement", exception.Message);
+            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => button.ClickAsync(new ClickOptions { Force = true }));
+            Assert.Contains("Element is not visible", exception.Message);
         }
 
         ///<playwright-file>elementhandle.spec.js</playwright-file>
         ///<playwright-describe>ElementHandle.click</playwright-describe>
-        ///<playwright-it>should throw for recursively hidden nodes</playwright-it>
+        ///<playwright-it>should throw for recursively hidden nodes with force</playwright-it>
         [Retry]
-        public async Task ShouldThrowForRecursivelyHiddenNodes()
+        public async Task ShouldThrowForRecursivelyHiddenNodesWithForce()
         {
             await Page.GoToAsync(TestConstants.ServerUrl + "/input/button.html");
             var button = await Page.QuerySelectorAsync("button");
             await Page.EvaluateAsync("button => button.parentElement.style.display = 'none'", button);
-            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => button.ClickAsync());
-            Assert.Equal("Node is either not visible or not an HTMLElement", exception.Message);
+            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => button.ClickAsync(new ClickOptions { Force = true }));
+            Assert.Contains("Element is not visible", exception.Message);
         }
 
         ///<playwright-file>elementhandle.spec.js</playwright-file>
         ///<playwright-describe>ElementHandle.click</playwright-describe>
-        ///<playwright-it>should throw for &lt;br&gt; elements</playwright-it>
+        ///<playwright-it>should throw for &lt;br&gt; elements with force</playwright-it>
         [Retry]
-        public async Task ShouldThrowForBrElements()
+        public async Task ShouldThrowForBrElementsWithforce()
         {
             await Page.SetContentAsync("hello<br>goodbye");
             var br = await Page.QuerySelectorAsync("br");
-            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => br.ClickAsync());
-            Assert.Equal("Node is either not visible or not an HTMLElement", exception.Message);
+            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => br.ClickAsync(new ClickOptions { Force = true }));
+            Assert.Contains("Element is outside of the viewport", exception.Message);
+        }
+
+        ///<playwright-file>elementhandle.spec.js</playwright-file>
+        ///<playwright-describe>ElementHandle.click</playwright-describe>
+        ///<playwright-it>should double click the button</playwright-it>
+        [Retry]
+        public async Task ShouldDoubleClickTheButton()
+        {
+            await Page.GoToAsync(TestConstants.ServerUrl + "/input/button.html");
+            await Page.EvaluateAsync(@"() => {
+              window.double = false;
+              const button = document.querySelector('button');
+              button.addEventListener('dblclick', event => {
+                window.double = true;
+              });
+            }");
+
+            var button = await Page.QuerySelectorAsync("button");
+            await button.DoubleClickAsync();
+
+            Assert.True(await Page.EvaluateAsync<bool>("double"));
+            Assert.Equal("Clicked", await Page.EvaluateAsync<string>("() => result"));
         }
     }
 }
