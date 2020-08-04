4d8c8d012afb3be6dd9875153b4fc62246dcd1a0
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-08-04 20:46:39 -0300

    Pass viewport emulation tests (#688)
    related to #499

---
+++ src/PlaywrightSharp.Tests/Emulation/ViewportIsMobileTests.cs
@@ -0,0 +1,226 @@
+using System.Threading.Tasks;
+using PlaywrightSharp.Tests.Attributes;
+using PlaywrightSharp.Tests.BaseTests;
+using PlaywrightSharp.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.Emulation
+{
+    ///<playwright-file>emulation.spec.js</playwright-file>
+    ///<playwright-describe>viewport.isMobile</playwright-describe>
+    [Collection(TestConstants.TestFixtureBrowserCollectionName)]
+    public class ViewportIsMobileTests : PlaywrightSharpBrowserBaseTest
+    {
+        private readonly BrowserContextOptions _iPhone;
+        private readonly BrowserContextOptions _iPhoneLandscape;
+
+        /// <inheritdoc/>
+        public ViewportIsMobileTests(ITestOutputHelper output) : base(output)
+        {
+            _iPhone = Playwright.Devices["iPhone 6"];
+            _iPhoneLandscape = Playwright.Devices["iPhone 6 landscape"];
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should support mobile emulation</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldSupportMobileEmulation()
+        {
+            await using var context = await Browser.NewContextAsync(_iPhone);
+            var page = await context.NewPageAsync();
+
+            await page.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            Assert.Equal(375, await page.EvaluateAsync<int>("window.innerWidth"));
+            await page.SetViewportSizeAsync(new ViewportSize { Width = 400, Height = 300 });
+            Assert.Equal(400, await page.EvaluateAsync<int>("window.innerWidth"));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should support touch emulation</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldSupportTouchEmulation()
+        {
+            const string dispatchTouch = @"
+            function dispatchTouch() {
+              let fulfill;
+              const promise = new Promise(x => fulfill = x);
+              window.ontouchstart = function(e) {
+                fulfill('Received touch');
+              };
+              window.dispatchEvent(new Event('touchstart'));
+
+              fulfill('Did not receive touch');
+
+              return promise;
+            }";
+
+            await using var context = await Browser.NewContextAsync(_iPhone);
+            var page = await context.NewPageAsync();
+            await page.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            Assert.True(await page.EvaluateAsync<bool>("'ontouchstart' in window"));
+            Assert.Equal("Received touch", await page.EvaluateAsync<string>(dispatchTouch));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should be detectable by Modernizr</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldBeDetectableByModernizr()
+        {
+            await using var context = await Browser.NewContextAsync(_iPhone);
+            var page = await context.NewPageAsync();
+            await page.GoToAsync(TestConstants.ServerUrl + "/detect-touch.html");
+            Assert.Equal("YES", await page.EvaluateAsync<string>("document.body.textContent.trim()"));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should detect touch when applying viewport with touches</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldDetectTouchWhenApplyingViewportWithTouches()
+        {
+            await using var context = await Browser.NewContextAsync(new BrowserContextOptions
+            {
+                Viewport = new ViewportSize
+                {
+                    Width = 800,
+                    Height = 600,
+                },
+                HasTouch = true,
+            });
+
+            var page = await context.NewPageAsync();
+            await page.GoToAsync(TestConstants.EmptyPage);
+            await page.AddScriptTagAsync(new AddTagOptions
+            {
+                Url = TestConstants.ServerUrl + "/modernizr.js"
+            });
+            Assert.True(await page.EvaluateAsync<bool>("() => Modernizr.touchevents"));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should support landscape emulation</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldSupportLandscapeEmulation()
+        {
+            await using var context1 = await Browser.NewContextAsync(_iPhone);
+            var page1 = await context1.NewPageAsync();
+            await page1.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            Assert.False(await page1.EvaluateAsync<bool>("() => matchMedia('(orientation: landscape)').matches"));
+
+            await using var context2 = await Browser.NewContextAsync(_iPhoneLandscape);
+            var page2 = await context2.NewPageAsync();
+            await page2.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            Assert.True(await page2.EvaluateAsync<bool>("() => matchMedia('(orientation: landscape)').matches"));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should support window.orientation emulation</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldSupportWindowOrientationEmulation()
+        {
+            await using var context = await Browser.NewContextAsync(new BrowserContextOptions
+            {
+                Viewport = new ViewportSize
+                {
+                    Width = 300,
+                    Height = 400,
+                },
+                IsMobile = true,
+            });
+            var page = await context.NewPageAsync();
+            await page.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            Assert.Equal(0, await page.EvaluateAsync<int?>("() => window.orientation"));
+
+            await page.SetViewportSizeAsync(new ViewportSize { Width = 400, Height = 300 });
+            Assert.Equal(90, await page.EvaluateAsync<int?>("() => window.orientation"));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>should fire orientationchange event</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task ShouldFireOrientationChangeEvent()
+        {
+            await using var context = await Browser.NewContextAsync(new BrowserContextOptions
+            {
+                Viewport = new ViewportSize
+                {
+                    Width = 300,
+                    Height = 400,
+                },
+                IsMobile = true,
+            });
+            var page = await context.NewPageAsync();
+            await page.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            await page.EvaluateAsync(@"() => {
+                window.counter = 0;
+                window.addEventListener('orientationchange', () => console.log(++window.counter));
+            }");
+
+            var event1Task = page.WaitForEvent<ConsoleEventArgs>(PageEvent.Console);
+            await page.SetViewportSizeAsync(new ViewportSize
+            {
+                Width = 400,
+                Height = 300,
+            });
+            var event1 = await event1Task;
+            Assert.Equal("1", event1.Message.Text);
+
+            var event2Task = page.WaitForEvent<ConsoleEventArgs>(PageEvent.Console);
+            await page.SetViewportSizeAsync(new ViewportSize
+            {
+                Width = 300,
+                Height = 400,
+            });
+            var event2 = await event2Task;
+            Assert.Equal("2", event2.Message.Text);
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>default mobile viewports to 980 width</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task DefaultMobileViewportsTo980Width()
+        {
+            await using var context = await Browser.NewContextAsync(new BrowserContextOptions
+            {
+                Viewport = new ViewportSize
+                {
+                    Width = 320,
+                    Height = 480,
+                },
+                IsMobile = true,
+            });
+            var page = await context.NewPageAsync();
+
+            await page.GoToAsync(TestConstants.EmptyPage);
+            Assert.Equal(980, await page.EvaluateAsync<int>("() => window.innerWidth"));
+        }
+
+        ///<playwright-file>emulation.spec.js</playwright-file>
+        ///<playwright-describe>viewport.isMobile</playwright-describe>
+        ///<playwright-it>respect meta viewport tag</playwright-it>
+        [SkipBrowserAndPlatformFact(skipFirefox: true)]
+        public async Task RespectMetaViewportTag()
+        {
+            await using var context = await Browser.NewContextAsync(new BrowserContextOptions
+            {
+                Viewport = new ViewportSize
+                {
+                    Width = 320,
+                    Height = 480,
+                },
+                IsMobile = true,
+            });
+            var page = await context.NewPageAsync();
+            await page.GoToAsync(TestConstants.ServerUrl + "/mobile.html");
+            Assert.Equal(320, await page.EvaluateAsync<int>("() => window.innerWidth"));
+        }
+    }
+}
--- src/PlaywrightSharp/DeviceDescriptor.cs
+++ src/PlaywrightSharp/DeviceDescriptor.cs
@@ -16,13 +16,50 @@ namespace PlaywrightSharp
         public string UserAgent { get; set; }
 
         /// <summary>
-        /// ViewPort.
+        /// Viewport.
         /// </summary>
-        public ViewportSize ViewPort { get; set; }
+        public ViewportSize Viewport { get; set; }
 
         /// <summary>
         /// Has touch.
         /// </summary>
         public bool HasTouch { get; set; }
+
+        /// <summary>
+        /// Is mobile.
+        /// </summary>
+        public bool IsMobile { get; set; }
+
+        /// <summary>
+        /// Device scale factor.
+        /// </summary>
+        public double DeviceScaleFactor { get; set; }
+
+        /// <summary>
+        /// Converts the <see cref="DeviceDescriptor"/> to <see cref="BrowserContextOptions"/>.
+        /// </summary>
+        /// <param name="descriptor">Descriptor to convert.</param>
+        public static implicit operator BrowserContextOptions(DeviceDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                return null;
+            }
+
+            return new BrowserContextOptions()
+            {
+                UserAgent = descriptor.UserAgent,
+                Viewport = descriptor.Viewport,
+                HasTouch = descriptor.HasTouch,
+                IsMobile = descriptor.IsMobile,
+                DeviceScaleFactor = descriptor.DeviceScaleFactor,
+            };
+        }
+
+        /// <summary>
+        /// Converts the <see cref="BrowserContextOptions"/> to <see cref="BrowserContextOptions"/>.
+        /// </summary>
+        /// <returns>A <see cref="BrowserContextOptions"/> with the same information as the <see cref="DeviceDescriptor"/>.</returns>
+        public BrowserContextOptions ToBrowserContextOptions() => this;
     }
 }
