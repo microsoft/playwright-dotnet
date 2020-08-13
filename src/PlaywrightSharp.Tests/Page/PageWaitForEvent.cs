bf570214d3ec5960229c17f1d4cee8e17c71abfe
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-08-13 19:34:15 -0300

    Pass PageWaitForEvent
    closes #520

---
+++ src/PlaywrightSharp.Tests/Page/PageWaitForEvent.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using PlaywrightSharp.Tests.BaseTests;
+using PlaywrightSharp.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.Page
+{
+    ///<playwright-file>page.spec.js</playwright-file>
+    ///<playwright-describe>Page.waitForEvent</playwright-describe>
+    [Collection(TestConstants.TestFixtureBrowserCollectionName)]
+    public class PageWaitForEvent : PlaywrightSharpPageBaseTest
+    {
+        /// <inheritdoc/>
+        public PageWaitForEvent(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        ///<playwright-file>page.spec.js</playwright-file>
+        ///<playwright-describe>Page.waitForEvent</playwright-describe>
+        ///<playwright-it>should work</playwright-it>
+        [Fact(Timeout = PlaywrightSharp.Playwright.DefaultTimeout)]
+        public async Task ShouldFailWithErrorUponDisconnect()
+        {
+            var task = Page.WaitForEvent<DownloadEventArgs>(PageEvent.Download);
+            await Page.CloseAsync();
+            var exception = await Assert.ThrowsAnyAsync<PlaywrightSharpException>(() => task);
+            Assert.Contains("Page closed", exception.Message);
+        }
+    }
+}
