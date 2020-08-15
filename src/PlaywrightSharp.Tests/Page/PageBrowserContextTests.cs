d9ec8de16e6a2c46f29d2ead24b076638aa0aa41
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-08-15 16:30:36 -0300

    Pass PageBrowserContextTests (#734)
    related to #520

--- src/PlaywrightSharp.Tests/Page/PageBrowserContextTests.cs
+++ src/PlaywrightSharp.Tests/Page/PageBrowserContextTests.cs
@@ -1,5 +1,4 @@
 using PlaywrightSharp.Tests.BaseTests;
-using PlaywrightSharp.Tests.Helpers;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -8,8 +7,7 @@ namespace PlaywrightSharp.Tests.Page
     ///<playwright-file>page.spec.js</playwright-file>
     ///<playwright-describe>Page.browserContext</playwright-describe>
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1000:Test classes must be public", Justification = "Disabled")]
-    class PageBrowserContextTests : PlaywrightSharpPageBaseTest
+    public class PageBrowserContextTests : PlaywrightSharpPageBaseTest
     {
         /// <inheritdoc/>
         public PageBrowserContextTests(ITestOutputHelper output) : base(output)
