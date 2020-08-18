0d57fe7aa7470ddbf2a46f8d9e395b5ace050db8
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-08-18 19:02:36 -0300

    Pass RequestHeadersTests (#755)
    related to #519

--- src/PlaywrightSharp.Tests/Page/Network/RequestHeadersTests.cs
+++ src/PlaywrightSharp.Tests/Page/Network/RequestHeadersTests.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using PlaywrightSharp.Tests.BaseTests;
 using PlaywrightSharp.Tests.Helpers;
 using Xunit;
@@ -9,8 +12,7 @@ namespace PlaywrightSharp.Tests.Page.Network
     ///<playwright-file>network.spec.js</playwright-file>
     ///<playwright-describe>Request.headers</playwright-describe>
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1000:Test classes must be public", Justification = "Disabled")]
-    class RequestHeadersTests : PlaywrightSharpPageBaseTest
+    public class RequestHeadersTests : PlaywrightSharpPageBaseTest
     {
         /// <inheritdoc/>
         public RequestHeadersTests(ITestOutputHelper output) : base(output)
@@ -32,7 +34,15 @@ namespace PlaywrightSharp.Tests.Page.Network
                     TestConstants.WebkitProduct => "WebKit",
                     _ => "None"
                 },
-                response.Request.Headers["User-Agent"]);
+                response.Request.Headers["user-agent"]);
+        }
+
+        ///<playwright-file>network.spec.js</playwright-file>
+        ///<playwright-describe>Request.headers</playwright-describe>
+        ///<playwright-it>should get the same headers as the server</playwright-it>
+        [Fact(Skip = "We don't need to test this")]
+        public void ShouldGetTheSameHeadersAsTheServer()
+        {
         }
     }
 }
