c41c3f6679863ab35838ddf3cf2ae6b765a39ad6
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-08-18 09:45:15 -0300

    Pass RequestPostDataJsonTests
    related to #519

--- src/PlaywrightSharp/Request.cs
+++ src/PlaywrightSharp/Request.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web;
 using PlaywrightSharp.Transport;
 using PlaywrightSharp.Transport.Channels;
 using PlaywrightSharp.Transport.Protocol;
@@ -60,5 +63,59 @@ namespace PlaywrightSharp
 
         /// <inheritdoc />
         public async Task<IResponse> GetResponseAsync() => (await _channel.GetResponseAsync().ConfigureAwait(false))?.Object;
+
+        /// <inheritdoc />
+        public JsonDocument GetJsonAsync(JsonDocumentOptions options = default)
+        {
+            string content = GetRequestForJson();
+
+            if (content == null)
+            {
+                return null;
+            }
+
+            return JsonDocument.Parse(content, options);
+        }
+
+        /// <inheritdoc />
+        public T GetJsonAsync<T>(JsonSerializerOptions options = null)
+        {
+            string content = GetRequestForJson();
+
+            if (content == null)
+            {
+                return default;
+            }
+
+            return JsonSerializer.Deserialize<T>(content, options ?? _channel.Scope.Connection.GetDefaultJsonSerializerOptions());
+        }
+
+        private string GetRequestForJson()
+        {
+            if (_initializer.PostData == null)
+            {
+                return null;
+            }
+
+            if (Headers.TryGetValue("content-type", out string contentType) && string.IsNullOrEmpty(contentType))
+            {
+                return null;
+            }
+
+            if (contentType == "application/x-www-form-urlencoded")
+            {
+                var parsed = HttpUtility.ParseQueryString(_initializer.PostData);
+                var dictionary = new Dictionary<string, string>();
+
+                foreach (string key in parsed.Keys)
+                {
+                    dictionary[key] = parsed[key];
+                }
+
+                return JsonSerializer.Serialize(dictionary);
+            }
+
+            return _initializer.PostData;
+        }
     }
 }
