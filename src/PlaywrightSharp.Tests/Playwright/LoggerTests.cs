8fbc1692d772ff553ca86be0c04332b1cece5071
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-08-20 08:51:51 -0300

    Pass LoggerTests (#761)
    * Pass LoggerTests
    
    closes #515
    
    * code factor

---
+++ src/PlaywrightSharp.Tests/Playwright/LoggerTests.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Threading.Tasks;
+using PlaywrightSharp.Tests.Attributes;
+using PlaywrightSharp.Tests.BaseTests;
+using PlaywrightSharp.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.Launcher
+{
+    ///<playwright-file>logger.spec.js</playwright-file>
+    ///<playwright-describe>Logger</playwright-describe>
+    [Collection(TestConstants.TestFixtureBrowserCollectionName)]
+    public class LoggerTests : PlaywrightSharpBaseTest
+    {
+        /// <inheritdoc/>
+        public LoggerTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        ///<playwright-file>logger.spec.js</playwright-file>
+        ///<playwright-describe>Logger</playwright-describe>
+        ///<playwright-it>should log</playwright-it>
+        [Fact(Skip = "We don't test CHANNEL")]
+        public void ShouldLog()
+        {
+        }
+
+        ///<playwright-file>logger.spec.js</playwright-file>
+        ///<playwright-describe>Logger</playwright-describe>
+        ///<playwright-it>should log context-level</playwright-it>
+        [Fact(Skip = "We don't test CHANNEL")]
+        public void ShouldLogContextLevel()
+        {
+        }
+    }
+}
