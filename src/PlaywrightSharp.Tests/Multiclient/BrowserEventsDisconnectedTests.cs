263a604868ed17c06f3581419a6534815029f39d
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-08-21 08:11:40 -0300

    Pass BrowserEventsDisconnected  (#766)
    * Pass BrowserEventsDisconnected
    
    related to #517
    
    * Rename file
    
    * rename class

---
+++ src/PlaywrightSharp.Tests/Multiclient/BrowserEventsDisconnectedTests.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading.Tasks;
+using PlaywrightSharp.Tests.BaseTests;
+using PlaywrightSharp.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.Multiclient
+{
+    ///<playwright-file>multiclient.spec.js</playwright-file>
+    ///<playwright-describe>Browser.Events.Disconnected</playwright-describe>
+    [Collection(TestConstants.TestFixtureBrowserCollectionName)]
+    public class BrowserEventsDisconnectedTests : PlaywrightSharpBrowserBaseTest
+    {
+        /// <inheritdoc/>
+        public BrowserEventsDisconnectedTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        ///<playwright-file>multiclient.spec.js</playwright-file>
+        ///<playwright-describe>Browser.Events.Disconnected</playwright-describe>
+        ///<playwright-it>should be emitted when: browser gets closed, disconnected or underlying websocket gets closed</playwright-it>
+        [Fact(Timeout = PlaywrightSharp.Playwright.DefaultTimeout)]
+        public async Task ShouldBeEmittedWhenBrowserGetsClosedDisconnectedOrUnderlyingWebsocketGetsClosed()
+        {
+            await using var browserServer = await BrowserType.LaunchServerAsync(TestConstants.GetDefaultBrowserOptions());
+            await using var originalBrowser = await BrowserType.ConnectAsync(new ConnectOptions { WSEndpoint = browserServer.WSEndpoint });
+            string wsEndpoint = browserServer.WSEndpoint;
+
+            await using var remoteBrowser1 = await BrowserType.ConnectAsync(new ConnectOptions { WSEndpoint = wsEndpoint });
+            await using var remoteBrowser2 = await BrowserType.ConnectAsync(new ConnectOptions { WSEndpoint = wsEndpoint });
+
+            int disconnectedOriginal = 0;
+            int disconnectedRemote1 = 0;
+            int disconnectedRemote2 = 0;
+
+            var originalBrowserDisconnectedTcs = new TaskCompletionSource<bool>();
+            var remote1DisconnectedTcs = new TaskCompletionSource<bool>();
+            var remote2DisconnectedTcs = new TaskCompletionSource<bool>();
+
+            originalBrowser.Disconnected += (sender, e) =>
+            {
+                ++disconnectedOriginal;
+                originalBrowserDisconnectedTcs.TrySetResult(true);
+            };
+
+            remoteBrowser1.Disconnected += (sender, e) =>
+            {
+                ++disconnectedRemote1;
+                remote1DisconnectedTcs.TrySetResult(true);
+            };
+
+            remoteBrowser2.Disconnected += (sender, e) =>
+            {
+                ++disconnectedRemote2;
+                remote2DisconnectedTcs.TrySetResult(true);
+            };
+
+            await TaskUtils.WhenAll(
+                remote2DisconnectedTcs.Task,
+                remoteBrowser2.CloseAsync());
+
+            Assert.Equal(0, disconnectedOriginal);
+            Assert.Equal(0, disconnectedRemote1);
+            Assert.Equal(1, disconnectedRemote2);
+
+            await TaskUtils.WhenAll(
+                originalBrowserDisconnectedTcs.Task,
+                remote1DisconnectedTcs.Task,
+                browserServer.CloseAsync());
+
+            Assert.Equal(1, disconnectedOriginal);
+            Assert.Equal(1, disconnectedRemote1);
+            Assert.Equal(1, disconnectedRemote2);
+        }
+    }
+}
