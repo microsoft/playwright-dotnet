2a43db9aef495bf99c5b8186393b610e6e6ec672
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-08-22 13:21:25 -0300

    Pass Attribute and Css selectors
    closes #524

---
+++ src/PlaywrightSharp.Tests/QuerySelector/AttributeSelectorTests.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Threading.Tasks;
+using PlaywrightSharp.Tests.BaseTests;
+using PlaywrightSharp.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PlaywrightSharp.Tests.QuerySelector
+{
+    ///<playwright-file>queryselector.spec.js</playwright-file>
+    ///<playwright-describe>attribute selector</playwright-describe>
+    [Collection(TestConstants.TestFixtureBrowserCollectionName)]
+    public class AttributeSelectorTests : PlaywrightSharpPageBaseTest
+    {
+        /// <inheritdoc/>
+        public AttributeSelectorTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        ///<playwright-file>queryselector.spec.js</playwright-file>
+        ///<playwright-describe>css selector</playwright-describe>
+        ///<playwright-it>should work for open shadow roots</playwright-it>
+        [Fact(Timeout = PlaywrightSharp.Playwright.DefaultTimeout)]
+        public async Task ShouldWorkForOpenShadowRoots()
+        {
+            await Page.GoToAsync(TestConstants.ServerUrl + "/deep-shadow.html");
+            Assert.Equal("Hello from root2", await Page.QuerySelectorEvaluateAsync<string>("id=target", "e => e.textContent"));
+            Assert.Equal("Hello from root1", await Page.QuerySelectorEvaluateAsync<string>("data-testid=foo", "e => e.textContent"));
+            Assert.Equal(3, await Page.QuerySelectorAllEvaluateAsync<int>("data-testid=foo", "els => els.length"));
+            Assert.Null(await Page.QuerySelectorAsync("id:light=target"));
+            Assert.Null(await Page.QuerySelectorAsync("data-testid:light=foo"));
+            Assert.Empty(await Page.QuerySelectorAllAsync("data-testid:light=foo"));
+        }
+    }
+}
--- src/PlaywrightSharp.Tests/QuerySelector/SelectorsRegisterTests.cs
+++ src/PlaywrightSharp.Tests/QuerySelector/SelectorsRegisterTests.cs
@@ -6,71 +6,46 @@ using Xunit.Abstractions;
 
 namespace PlaywrightSharp.Tests.QuerySelector
 {
-    /*
     ///<playwright-file>queryselector.spec.js</playwright-file>
     ///<playwright-describe>selectors.register</playwright-describe>
     [Collection(TestConstants.TestFixtureBrowserCollectionName)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1000:Test classes must be public", Justification = "Disabled")]class SelectorsRegisterTests : PlaywrightSharpPageBaseTest
+    public class SelectorsRegisterTests : PlaywrightSharpPageBaseTest
     {
         /// <inheritdoc/>
         public SelectorsRegisterTests(ITestOutputHelper output) : base(output)
         {
         }
 
-        internal ISelectors Selectors { get; set; } = PlaywrightSharp.Selectors.Instance.Value;
-
         ///<playwright-file>queryselector.spec.js</playwright-file>
         ///<playwright-describe>selectors.register</playwright-describe>
         ///<playwright-it>should work</playwright-it>
-        [Fact(Timeout = PlaywrightSharp.Playwright.DefaultTimeout)]
-        public async Task ShouldWork()
+        [Fact(Skip = "We are not going to support this on v1.1.1")]
+        public void ShouldWork()
         {
-            string createTagSelector = @"() => ({
-                name: 'tag',
-                create(root, target) {
-                    return target.nodeName;
-                },
-                query(root, selector) {
-                    return root.querySelector(selector);
-                },
-                queryAll(root, selector) {
-                    return Array.from(root.querySelectorAll(selector));
-                }
-            })";
-            await Selectors.RegisterAsync($"({createTagSelector})()");
-            await Page.SetContentAsync("<div><span></span></div><div></div>");
-            Assert.Equal("DIV", await Selectors.CreateSelectorAsync("tag", await Page.QuerySelectorAsync("div")));
-            Assert.Equal("DIV", await Page.QuerySelectorEvaluateAsync<string>("tag=DIV", "e => e.nodeName"));
-            Assert.Equal("SPAN", await Page.QuerySelectorEvaluateAsync<string>("tag=SPAN", "e => e.nodeName"));
-            Assert.Equal(2, await Page.QuerySelectorAllEvaluateAsync<int>("tag=DIV", "es => es.length"));
         }
 
         ///<playwright-file>queryselector.spec.js</playwright-file>
         ///<playwright-describe>selectors.register</playwright-describe>
-        ///<playwright-it>should update</playwright-it>
-        [Fact(Timeout = PlaywrightSharp.Playwright.DefaultTimeout)]
-        public async Task ShouldUpdate()
+        ///<playwright-it>should work with path</playwright-it>
+        [Fact(Skip = "We are not going to support this on v1.1.1")]
+        public void ShouldWorkWithPath()
         {
-            await Page.SetContentAsync("<div><dummy id=d1></dummy></div><span><dummy id=d2></dummy></span>");
-            Assert.Equal("DIV", await Page.QuerySelectorEvaluateAsync<string>("div", "e => e.nodeName"));
-            var exception = await Assert.ThrowsAsync<PlaywrightSharpException>(() => Page.QuerySelectorAsync("dummy=foo"));
-            Assert.Contains("Unknown engine dummy while parsing selector dummy=foo", exception.Message);
-            string createDummySelector = @"(name) => ({
-                name,
-                create(root, target) {
-                    return target.nodeName;
-                },
-                query(root, selector) {
-                    return root.querySelector(name);
-                },
-                queryAll(root, selector) {
-                    return Array.from(root.querySelectorAll(name));
-                }
-            })";
-            await Selectors.RegisterAsync(createDummySelector, "dummy");
-            Assert.Equal("d1", await Page.QuerySelectorEvaluateAsync<string>("dummy=foo", "e => e.id"));
-            Assert.Equal("d2", await Page.QuerySelectorEvaluateAsync<string>("css=span >> dummy=foo", "e => e.id"));
+        }
+
+        ///<playwright-file>queryselector.spec.js</playwright-file>
+        ///<playwright-describe>selectors.register</playwright-describe>
+        ///<playwright-it>should work in main and isolated world</playwright-it>
+        [Fact(Skip = "We are not going to support this on v1.1.1")]
+        public void ShouldWorkInMainAndIsolatedWorld()
+        {
+        }
+
+        ///<playwright-file>queryselector.spec.js</playwright-file>
+        ///<playwright-describe>selectors.register</playwright-describe>
+        ///<playwright-it>should handle errors</playwright-it>
+        [Fact(Skip = "We are not going to support this on v1.1.1")]
+        public void ShouldHandleErrors()
+        {
         }
     }
-    */
 }
