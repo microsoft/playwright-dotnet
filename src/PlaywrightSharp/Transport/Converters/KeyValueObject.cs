65c878ccb8881f904de822c61a97df1d2afdc250
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-09-11 08:00:08 -0300

    Pass Popup tests

---
+++ src/PlaywrightSharp/Transport/Converters/KeyValueObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace PlaywrightSharp.Transport.Converters
+{
+    internal class KeyValueObject
+    {
+        public string K { get; set; }
+
+        public JsonElement V { get; set; }
+    }
+}
