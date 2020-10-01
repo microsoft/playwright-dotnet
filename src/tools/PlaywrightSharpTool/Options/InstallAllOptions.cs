2a3878f652c8e178dd514bd2fa7cfd71b2958dd1
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-10-01 09:55:44 -0300

    Fix grammar

--- src/tools/PlaywrightSharpTool/Options/InstallAllOptions.cs
+++ src/tools/PlaywrightSharpTool/Options/InstallAllOptions.cs
@@ -3,7 +3,7 @@ using CommandLine;
 
 namespace PlaywrightSharpTool.Options
 {
-    [Verb("install-all", HelpText = "Install the playwright driver and the required browsers. Pass --driverpath to specify the driver location and --browserpath to specify the browsers location")]
+    [Verb("install-all", HelpText = "Install the playwright driver and the required browsers. Pass --driverpath to specify the driver location and --browserpath to specify the location of the browsers.")]
     public class InstallAllOptions
     {
         [Option(Required = false, HelpText = "Browsers destination path. If not passed, the default shared location will be used.")]
