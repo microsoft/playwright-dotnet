2c89fed867f35990450eda3483953437a14ab9df
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-11-12 08:32:09 -0300

    Remove ApiChecker

--- src/tools/ApiChecker/PlaywrightArgument.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ApiChecker
-{
-    public class PlaywrightArgument
-    {
-        public PlaywrightType Type { get; set; }
-    }
-}
--- src/tools/ApiChecker/PlaywrightEntity.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ApiChecker
-{
-    internal class PlaywrightEntity
-    {
-        public Dictionary<string, PlaywrightMember> Members { get; set; }
-    }
-}
