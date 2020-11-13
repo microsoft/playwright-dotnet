e0df7bf40c2afc66ec715192d9d0f4a8e4e32bae
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-11-13 09:02:16 -0300

    Revert "Remove ApiChecker"
    This reverts commit 2c89fed867f35990450eda3483953437a14ab9df.

---
+++ src/tools/ApiChecker/PlaywrightArgument.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ApiChecker
+{
+    public class PlaywrightArgument
+    {
+        public PlaywrightType Type { get; set; }
+    }
+}
---
+++ src/tools/ApiChecker/PlaywrightEntity.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ApiChecker
+{
+    internal class PlaywrightEntity
+    {
+        public Dictionary<string, PlaywrightMember> Members { get; set; }
+    }
+}
