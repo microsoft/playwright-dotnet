4de1ada5ff753a66b58f3557fddc591039e6f487
Dario Kondratiuk <dariokondratiuk@gmail.com>
2020-11-13 10:51:53 -0300

    Update driver to 0.161.0

--- src/tools/DriverDownloader/Program.cs
+++ src/tools/DriverDownloader/Program.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using DriverDownloader.Linux;
 
@@ -53,11 +55,59 @@ namespace DriverDownloader
                 }
 
                 await Task.WhenAll(tasks);
+                UpdateApiJson(destinationDirectory);
             }
             else
             {
                 Console.WriteLine("Drivers are up-to-date");
             }
+        }
+
+        private static void UpdateApiJson(DirectoryInfo destinationDirectory)
+        {
+            StringBuilder output = new StringBuilder();
+            var process = new Process()
+            {
+                StartInfo =
+                {
+                    FileName = GetDriverPath(destinationDirectory),
+                    Arguments = $"print-api-json",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                },
+            };
+
+            process.OutputDataReceived += (_, e) => output.Append(e.Data);
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+
+            File.WriteAllText(
+                Path.Combine(destinationDirectory.FullName, "..", "..", "tools", "ApiChecker", "api.json"),
+                output.ToString());
+        }
+
+        private static string GetDriverPath(DirectoryInfo directory)
+        {
+            string driversPath;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                driversPath = Path.Combine(directory.FullName, "win-x64", "playwright-cli.exe");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                driversPath = Path.Combine(directory.FullName, "osx", "playwright-cli");
+            }
+            else
+            {
+                driversPath = Path.Combine(directory.FullName, "unix", "playwright-cli");
+            }
+
+            return driversPath;
         }
 
         private static async Task DownloadDriverAsync(DirectoryInfo destinationDirectory, string driverVersion, string platform, string runtime)
@@ -74,6 +124,12 @@ namespace DriverDownloader
             }
 
             var directory = new DirectoryInfo(Path.Combine(destinationDirectory.FullName, runtime));
+
+            if (directory.Exists)
+            {
+                directory.Delete(true);
+            }
+
             new ZipArchive(await response.Content.ReadAsStreamAsync()).ExtractToDirectory(directory.FullName);
 
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
