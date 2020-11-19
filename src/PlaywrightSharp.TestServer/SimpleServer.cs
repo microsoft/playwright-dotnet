b1c2a626d6e3dfde9132e29cb63d9e9a8f7368af
Darío Kondratiuk <dariokondratiuk@gmail.com>
2020-11-19 10:34:25 -0300

    remove comments

--- src/PlaywrightSharp.TestServer/SimpleServer.cs
+++ src/PlaywrightSharp.TestServer/SimpleServer.cs
@@ -268,12 +268,6 @@ namespace PlaywrightSharp.TestServer
         {
             int id = Interlocked.Increment(ref counter);
 
-            // it's very unlikely that we reach the uint limit of 2 billion.
-            // even with 1 new connection per second, this would take 68 years.
-            // -> but if it happens, then we should throw an exception because
-            //    the caller probably should stop accepting clients.
-            // -> it's hardly worth using 'bool Next(out id)' for that case
-            //    because it's just so unlikely.
             if (id == int.MaxValue)
             {
                 throw new Exception("connection id limit reached: " + id);
