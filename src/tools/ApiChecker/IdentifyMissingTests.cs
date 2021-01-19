8dc34261ab53c976281c3a33993ec74593f4cf7f
Anze Vodovnik <anvod@microsoft.com>
2021-01-19 14:59:57 +0100

    Identify tests that have the same name, but not the right file.

--- src/tools/ApiChecker/IdentifyMissingTests.cs
+++ src/tools/ApiChecker/IdentifyMissingTests.cs
@@ -82,24 +82,64 @@ namespace ApiChecker
                 type => type.GetMethods().SelectMany(
                     method => method.GetCustomAttributes<PlaywrightSharp.Tests.PlaywrightTestAttribute>()));
 
-            var comparableList = attributes.Select(x => (x.FileName, x.TestName)).OrderBy(x => x.FileName).ToArray();
+            int potentialMatches = 0;
+            int fullMatches = 0;
+            int noMatches = 0;
+            int totalTests = 0;
 
-            int matchingTests = 0;
-            int missingTests = 0;
+            List<(string, string)> missingTests = new();
+            List<KeyValuePair<(string FileName, string TestName), List<(string, string)>>> invalidMaps = new();
             foreach (var atx in attributes)
             {
-                if (!_testPairs.Contains((atx.TrimmedName, atx.TestName)))
+                totalTests++;
+
+                // a test can either be a full match, a partial (i.e. just the test name) or no match
+                var potentialMatch = _testPairs.Where(x => string.Equals(x.TestName, atx.TestName, StringComparison.InvariantCultureIgnoreCase));
+                if (!potentialMatch.Any())
                 {
-                    Console.WriteLine($"Missing: {atx.FileName}: {atx.TestName}");
-                    missingTests++;
+                    noMatches++;
+                    missingTests.Add((atx.FileName, atx.TestName));
+                }
+                else if (potentialMatch.Count(x => string.Equals(x.FileName, atx.TrimmedName, StringComparison.InvariantCultureIgnoreCase)) == 1)
+                {
+                    fullMatches++;
+                    continue;
                 }
                 else
                 {
-                    matchingTests++;
+
+                    invalidMaps.Add(new KeyValuePair<(string FileName, string TestName), List<(string, string)>>((atx.TrimmedName, atx.TestName), potentialMatch.ToList()));
+                    potentialMatches++;
                 }
             }
 
-            Console.WriteLine($"There are {matchingTests} matching tests, but we're missing {missingTests}. We have {_testPairs.Count} tests.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Total matching tests: {fullMatches}/{totalTests}.");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Total tests found by name, but not by file: {potentialMatches}/{totalTests}.");
+            Console.ResetColor();
+
+            foreach (var invalidTest in invalidMaps)
+            {
+                Console.WriteLine($"{invalidTest.Key.FileName}: {invalidTest.Key.TestName}");
+                foreach (var items in invalidTest.Value)
+                {
+                    Console.WriteLine($"\t{items.Item1}: {items.Item2}");
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Total missing tests: {noMatches}/{totalTests}.");
+            Console.ResetColor();
+
+            foreach (var invalidTest in missingTests)
+            {
+                Console.WriteLine($"{invalidTest.Item1}: {invalidTest.Item2}");
+            }
+
+            Console.WriteLine($"Found/Mismatched/Missing: {fullMatches}/{potentialMatches}/{noMatches} out of {totalTests}");
         }
 
         private static readonly List<(string FileName, string TestName)> _testPairs = new();
