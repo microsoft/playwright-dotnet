6d9f299556a7fba605e555c2b7842ab2714780ad
Dario Kondratiuk <dariokondratiuk@gmail.com>
2021-01-21 10:20:24 -0300

    nit

--- src/tools/ApiChecker/Program.cs
+++ src/tools/ApiChecker/Program.cs
@@ -2,7 +2,7 @@
  * MIT License
  *
  * Copyright (c) Microsoft Corporation.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
