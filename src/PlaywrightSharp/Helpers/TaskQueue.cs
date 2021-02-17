96317888e348c87f8cd32318091a81485135f817
Simon Cropp <simon.cropp@gmail.com>
2021-02-17 23:06:29 +1100

    remove some methods that are not used

--- src/PlaywrightSharp/Helpers/TaskQueue.cs
+++ src/PlaywrightSharp/Helpers/TaskQueue.cs
@@ -15,19 +15,6 @@ namespace PlaywrightSharp.Helpers
             _semaphore.Dispose();
         }
 
-        internal async Task<T> EnqueueAsync<T>(Func<Task<T>> taskGenerator)
-        {
-            await _semaphore.WaitAsync().ConfigureAwait(false);
-            try
-            {
-                return await taskGenerator().ConfigureAwait(false);
-            }
-            finally
-            {
-                _semaphore.Release();
-            }
-        }
-
         internal async Task EnqueueAsync(Func<Task> taskGenerator)
         {
             await _semaphore.WaitAsync().ConfigureAwait(false);
