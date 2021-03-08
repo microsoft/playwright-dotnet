c575414013db1a4f45aab9432fb146a8adba60b7
Darío Kondratiuk <dariokondratiuk@gmail.com>
2021-03-08 08:51:41 -0300

    refactor(generator): IKeyboard (#1261)

--- src/PlaywrightSharp/Keyboard.cs
+++ src/PlaywrightSharp/Keyboard.cs
@@ -17,9 +17,9 @@ namespace PlaywrightSharp
 
         public Task UpAsync(string key) => _channel.KeyboardUpAsync(key);
 
-        public Task PressAsync(string key, int delay = 0) => _channel.PressAsync(key, delay);
+        public Task PressAsync(string key, float? delay) => _channel.PressAsync(key, delay ?? 0);
 
-        public Task TypeAsync(string text, int delay = 0) => _channel.TypeAsync(text, delay);
+        public Task TypeAsync(string text, float? delay) => _channel.TypeAsync(text, delay ?? 0);
 
         public Task InsertTextAsync(string text) => _channel.InsertTextAsync(text);
     }
