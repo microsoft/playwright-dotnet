5dd209beb68f52913697be734fa30b67d49fd22e
Pavel Feldman <pavel.feldman@gmail.com>
2021-05-26 07:26:04 -0700

    chore: generate initializers from protocol

---
+++ src/Playwright/Transport/Protocol/Generated/AndroidDeviceInitializer.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Microsoft.Playwright.Transport.Protocol
+{
+    internal class AndroidDeviceInitializer
+    {
+        public string Model { get; set; }
+
+        public string Serial { get; set; }
+    }
+}
---
+++ src/Playwright/Transport/Protocol/Generated/StreamInitializer.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace Microsoft.Playwright.Transport.Protocol
+{
+    internal class StreamInitializer
+    {
+    }
+}
