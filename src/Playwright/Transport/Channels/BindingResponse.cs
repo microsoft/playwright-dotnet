1832eb03c8df60101dd44ade6147a8a27162a378
Anže Vodovnik <anvod@microsoft.com>
2021-06-14 10:12:48 +0200

    chore: cleaning up unused types (#1530)

--- src/Playwright/Transport/Channels/BindingResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace Microsoft.Playwright.Transport.Channels
-{
-    internal class BindingResponse
-    {
-        public object Result { get; set; }
-    }
-}
--- src/Playwright/Transport/Channels/PageChannelConsoleEventArgs.cs
+++
@@ -1,6 +0,0 @@
-namespace Microsoft.Playwright.Transport.Channels
-{
-    internal class PageChannelConsoleEventArgs
-    {
-    }
-}
