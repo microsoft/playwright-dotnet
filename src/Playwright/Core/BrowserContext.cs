b542217dbd76232c3fef605491ed4432daa39614
Simon Cropp <simon.cropp@gmail.com>
2021-06-16 10:59:02 +1000

    Update BrowserContext.cs

--- src/Playwright/Core/BrowserContext.cs
+++ src/Playwright/Core/BrowserContext.cs
@@ -156,9 +156,7 @@ namespace Microsoft.Playwright.Core
         public Task<IReadOnlyList<BrowserContextCookiesResult>> CookiesAsync(IEnumerable<string> urls = null) => Channel.CookiesAsync(urls);
 
         public Task ExposeBindingAsync(string name, Action callback, bool? handle = null)
-        {
-            return ExposeBindingAsync(name, (Delegate)callback, handle.GetValueOrDefault());
-        }
+            => ExposeBindingAsync(name, (Delegate)callback, handle.GetValueOrDefault());
 
         public Task ExposeBindingAsync(string name, Action<BindingSource> callback)
             => ExposeBindingAsync(name, (Delegate)callback);
