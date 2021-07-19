8b5a120092a98fa78e348dc1154302de8f4a3e2a
Archish Thakkar <archishthakkar@gmail.com>
2021-07-19 21:48:17 +0530

    :art: debug lines

--- src/Playwright.Tests/BrowserTypeConnectTests.cs
+++ src/Playwright.Tests/BrowserTypeConnectTests.cs
@@ -49,8 +49,8 @@ namespace Microsoft.Playwright.Tests
                 {
                     StartInfo =
                     {
-                        FileName = "dotnet",
-                        Arguments = $"{typeof(Playwright).Assembly.Location} launch-server {BrowserType.Name}",
+                        FileName = GetExecutablePath(),
+                        Arguments = $"launch-server {BrowserType.Name}",
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
                         RedirectStandardInput = true,
@@ -58,11 +58,15 @@ namespace Microsoft.Playwright.Tests
                         CreateNoWindow = true,
                     },
                 };
+                Console.WriteLine($"{browserServer.Process.StartInfo.FileName} {browserServer.Process.StartInfo.Arguments}");
                 browserServer.Process.Start();
                 browserServer.Process.Exited += (_, _) => browserServer.Process.Kill();
                 browserServer.WSEndpoint = browserServer.Process.StandardOutput.ReadLine();
 
-                if (!browserServer.WSEndpoint.StartsWith("ws://"))
+                Console.WriteLine($"{typeof(Playwright).Assembly.Location}");
+                Console.WriteLine($"{browserServer.WSEndpoint}");
+
+                if (browserServer.WSEndpoint != null && !browserServer.WSEndpoint.StartsWith("ws://"))
                 {
                     throw new PlaywrightException("Invalid web socket address: " + browserServer.WSEndpoint);
                 }
@@ -83,7 +87,7 @@ namespace Microsoft.Playwright.Tests
 
         [PlaywrightTest("browsertype-connect.spec.ts", "should be able to reconnect to a browser")]
         [Test, Timeout(TestConstants.DefaultTestTimeout)]
-        public async Task ShouldBeAbleToReconnectToBrowserAsync()
+        public async Task ShouldBeAbleToConnectToBrowserAsync()
         {
             var browser = await BrowserType.ConnectAsync(_browserServer.WSEndpoint);
             var context = await browser.NewContextAsync();
@@ -157,6 +161,58 @@ namespace Microsoft.Playwright.Tests
         {
             public Process Process { get; set; }
             public string WSEndpoint { get; set; }
+        }
+
+        private static string GetExecutablePath()
+        {
+            DirectoryInfo assemblyDirectory = new(AppContext.BaseDirectory);
+            if (!assemblyDirectory.Exists || !File.Exists(Path.Combine(assemblyDirectory.FullName, "Microsoft.Playwright.dll")))
+            {
+                var assemblyLocation = typeof(Playwright).Assembly.Location;
+                assemblyDirectory = new FileInfo(assemblyLocation).Directory;
+            }
+
+            string executableFile = GetPath(assemblyDirectory.FullName);
+            if (File.Exists(executableFile))
+            {
+                return executableFile;
+            }
+
+            // if the above fails, we can assume we're in the nuget registry
+            executableFile = GetPath(assemblyDirectory.Parent.Parent.FullName);
+            if (File.Exists(executableFile))
+            {
+                return executableFile;
+            }
+
+            throw new PlaywrightException($"Driver not found: {executableFile}");
+        }
+
+        private static string GetPath(string driversPath)
+        {
+            string platformId;
+            string runnerName;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                platformId = RuntimeInformation.OSArchitecture == Architecture.X64 ? "win32_x64" : "win";
+                runnerName = "playwright.cmd";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                runnerName = "playwright.sh";
+                platformId = "mac";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                runnerName = "playwright.sh";
+                platformId = "linux";
+            }
+            else
+            {
+                throw new PlaywrightException("Unknown platform");
+            }
+
+            return Path.Combine(driversPath, ".playwright", "node", platformId, runnerName);
         }
     }
 }
