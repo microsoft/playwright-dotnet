f5665715dfcbd33c13ff2cff231269a15407aed2
Anže Vodovnik <anvod@microsoft.com>
2021-08-24 18:54:57 +0200

    the tests should make sense

--- src/Playwright.TestingHarnessTest/SimpleSmokeTests.MSTest.cs
+++ src/Playwright.TestingHarnessTest/SimpleSmokeTests.MSTest.cs
@@ -1,30 +1,26 @@
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Playwright.MSTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Playwright.Tests.MSTest
+namespace Playwright.TestingHarnessTest.MSTest
 {
     [TestClass]
     [TestCategory("Smoke")]
-    public class SimpleSmokeTestsMSTest : PageTest
+    public class SimpleSmokeTests : PageTest
     {
         [TestMethod]
         public async Task ShouldOpenPlaywright()
         {
+            var path = Path.GetFullPath("index.html");
             Assert.IsNotNull(Page);
-            await Page.GotoAsync("https://www.playwright.dev");
+            await Page.GotoAsync("file://" + path);
             var h1 = await Page.TextContentAsync("h1");
-            Assert.AreEqual("Playwright enables reliable end-to-end testing for modern web apps.", h1);
+            Assert.AreEqual("Getting started.", h1);
+
+            var title = await Page.EvaluateAsync<string>("() => document.title");
+            Assert.AreEqual("This is a website.", title);
         }
 
-        [TestMethod]
-        public async Task ShouldNavigate()
-        {
-            Assert.IsNotNull(Page);
-            await Page.GotoAsync("https://www.playwright.dev");
-            await Page.ClickAsync("text=Get started");
-            await Page.WaitForLoadStateAsync(Microsoft.Playwright.LoadState.DOMContentLoaded);
-            Assert.AreEqual("Getting Started", await Page.TextContentAsync("h1"));
-        }
     }
 }
--- src/Playwright.TestingHarnessTest/SimpleSmokeTests.NUnit.cs
+++ src/Playwright.TestingHarnessTest/SimpleSmokeTests.NUnit.cs
@@ -1,29 +1,24 @@
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
-namespace Playwright.Tests.MSTest
+namespace Playwright.TestingHarnessTest.NUnit
 {
 
-    public class SimpleSmokeTestsNUnit : PageTest
+    public class SimpleSmokeTests : PageTest
     {
         [Test]
         public async Task ShouldOpenPlaywright()
         {
+            var path = Path.GetFullPath("index.html");
             Assert.IsNotNull(Page);
-            await Page.GotoAsync("https://www.playwright.dev");
+            await Page.GotoAsync("file://" + path);
             var h1 = await Page.TextContentAsync("h1");
-            Assert.AreEqual("Playwright enables reliable end-to-end testing for modern web apps.", h1);
-        }
+            Assert.AreEqual("Getting started.", h1);
 
-        [Test]
-        public async Task ShouldNavigate()
-        {
-            Assert.IsNotNull(Page);
-            await Page.GotoAsync("https://www.playwright.dev");
-            await Page.ClickAsync("text=Get started");
-            await Page.WaitForLoadStateAsync(Microsoft.Playwright.LoadState.DOMContentLoaded);
-            Assert.AreEqual("Getting Started", await Page.TextContentAsync("h1"));
+            var title = await Page.EvaluateAsync<string>("() => document.title");
+            Assert.AreEqual("This is a website.", title);
         }
     }
 }
