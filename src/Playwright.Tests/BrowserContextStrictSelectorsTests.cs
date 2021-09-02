906fea1e8e68497ac336fb2423fd9d474f892a4a
Anže Vodovnik <anvod@microsoft.com>
2021-09-02 14:58:43 +0200

    PR suggestions

--- src/Playwright.Tests/BrowserContextStrictSelectorsTests.cs
+++ src/Playwright.Tests/BrowserContextStrictSelectorsTests.cs
@@ -1,13 +1,12 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Darío Kondratiuk
- * Modifications copyright (c) Microsoft Corporation.
+ * Copyright (c) Microsoft Corporation.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
@@ -23,10 +22,7 @@
  * SOFTWARE.
  */
 
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
