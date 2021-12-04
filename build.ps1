53ccf22692aa4266f756fe0df89adb7b8a8775a9
Max Schmitt <max@schmitt.mx>
2021-12-03 16:33:54 -0800

    test: move upstream assets back in the repo (#1852)

--- build.ps1
+++ build.ps1
@@ -60,7 +60,7 @@ function Invoke-DownloadDriver() {
 
 function Invoke-WWWRoot() {
   Write-Host "🌐 Synchronizing wwwroot folder..."
-  Remove-Item -Path .\src\Playwright.Tests.TestServer\wwwroot\ -Recurse -ErrorAction Ignore
+  Remove-Item -Path .\src\Playwright.Tests.TestServer\wwwroot\ -Recurse -Force
   Copy-Item -Path .\playwright\tests\assets -Destination .\src\Playwright.Tests.TestServer\wwwroot\ -Recurse
 }
 
