7e23577fd23ad1251a8d8a9f7222f5a95feaee6a
Max Schmitt <max@schmitt.mx>
2022-02-25 17:15:29 +0100

    feat: added Docker publishing

---
+++ utils/docker/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+set +x
+
+if [[ ($1 == '--help') || ($1 == '-h') || ($1 == '') || ($2 == '') ]]; then
+  echo "usage: $(basename $0) {--arm64,--amd64} {bionic,focal} playwright:localbuild-focal"
+  echo
+  echo "Build Playwright docker image and tag it as 'playwright:localbuild-focal'."
+  echo "Once image is built, you can run it with"
+  echo ""
+  echo "  docker run --rm -it playwright:localbuild-focal /bin/bash"
+  echo ""
+  echo "NOTE: this requires on Playwright dependencies to be installed with 'npm install'"
+  echo "      and Playwright itself being built with 'npm run build'"
+  echo ""
+  exit 0
+fi
+
+function cleanup() {
+  rm -rf dist/
+}
+
+trap "cleanup; cd $(pwd -P)" EXIT
+cd "$(dirname "$0")"
+
+mkdir dist
+dotnet build ../../src/Playwright
+dotnet pack ../../src/Playwright -o dist/
+
+PLATFORM=""
+if [[ "$1" == "--arm64" ]]; then
+  PLATFORM="linux/arm64";
+elif [[ "$1" == "--amd64" ]]; then
+  PLATFORM="linux/amd64"
+else
+  echo "ERROR: unknown platform specifier - $1. Only --arm64 or --amd64 is supported"
+  exit 1
+fi
+
+docker build --platform "${PLATFORM}" -t "$3" -f "Dockerfile.$2" .
