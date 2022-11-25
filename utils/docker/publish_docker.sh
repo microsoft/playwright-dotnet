8333a7c6aa1ca8733b8922b0e7893d0795482966
Max Schmitt <max@schmitt.mx>
2022-11-24 15:23:37 -1000

    chore: introduce Docker Jammy images

--- utils/docker/publish_docker.sh
+++ utils/docker/publish_docker.sh
@@ -28,25 +28,24 @@ if [[ -z "${GITHUB_SHA}" ]]; then
   exit 1
 fi
 
-BIONIC_TAGS=(
-  "next-bionic"
-  "v${PW_VERSION}-bionic"
-)
-if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
-  BIONIC_TAGS+=("bionic")
-fi
-
 FOCAL_TAGS=(
   "next"
   "sha-${GITHUB_SHA}"
   "next-focal"
 )
-
 if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
   FOCAL_TAGS+=("latest")
   FOCAL_TAGS+=("focal")
   FOCAL_TAGS+=("v${PW_VERSION}-focal")
   FOCAL_TAGS+=("v${PW_VERSION}")
+fi
+
+JAMMY_TAGS=(
+  "next-jammy"
+)
+if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
+  JAMMY_TAGS+=("jammy")
+  JAMMY_TAGS+=("v${PW_VERSION}-jammy")
 fi
 
 tag_and_push() {
@@ -60,12 +59,12 @@ tag_and_push() {
 publish_docker_images_with_arch_suffix() {
   local FLAVOR="$1"
   local TAGS=()
-  if [[ "$FLAVOR" == "bionic" ]]; then
-    TAGS=("${BIONIC_TAGS[@]}")
-  elif [[ "$FLAVOR" == "focal" ]]; then
+  if [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
+  elif [[ "$FLAVOR" == "jammy" ]]; then
+    TAGS=("${_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic' or 'focal'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'focal' or 'jammy'"
     exit 1
   fi
   local ARCH="$2"
@@ -86,12 +85,12 @@ publish_docker_images_with_arch_suffix() {
 publish_docker_manifest () {
   local FLAVOR="$1"
   local TAGS=()
-  if [[ "$FLAVOR" == "bionic" ]]; then
-    TAGS=("${BIONIC_TAGS[@]}")
-  elif [[ "$FLAVOR" == "focal" ]]; then
+  if [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
+  elif [[ "$FLAVOR" == "jammy" ]]; then
+    TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic' or 'focal'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'focal' or 'jammy'"
     exit 1
   fi
 
