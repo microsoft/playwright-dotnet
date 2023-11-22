60fb62f94de550b7d5c3caffa1735bff718a2a3f
Max Schmitt <max@schmitt.mx>
2023-11-22 16:36:02 +0000

    devops: fix .NET publishing

--- utils/docker/build.sh
+++ utils/docker/build.sh
@@ -23,27 +23,19 @@ function cleanup() {
 trap "cleanup; cd $(pwd -P)" EXIT
 cd "$(dirname "$0")"
 
-# .NET internally has two sources when we install a tool with --add-source
-# so our local version needs to be higher that it gets priority over the remote one.
-# Also it should not include any pre-release versions (include next).
-xml_file_path="../../src/Common/Version.props"
-xml_file_contents=$(cat "${xml_file_path}")
-xml_file_contents=$(echo "${xml_file_contents}" | sed "s|<AssemblyVersion>.*</AssemblyVersion>|<AssemblyVersion>1.99.99</AssemblyVersion>|")
-xml_file_contents=$(echo "${xml_file_contents}" | sed "s|<PackageVersion>.*</PackageVersion>|<PackageVersion>1.99.99</PackageVersion>|")
-echo "${xml_file_contents}" > "${xml_file_path}"
-
-mkdir dist
-dotnet build ../../src/Playwright
-dotnet pack ../../src/Playwright -o dist/
-
-PLATFORM=""
+DOCKER_PLATFORM=""
+DOTNET_ARCH=""
 if [[ "$1" == "--arm64" ]]; then
-  PLATFORM="linux/arm64";
+  DOCKER_PLATFORM="linux/arm64";
+  DOTNET_ARCH="linux-arm64";
 elif [[ "$1" == "--amd64" ]]; then
-  PLATFORM="linux/amd64"
+  DOCKER_PLATFORM="linux/amd64"
+  DOTNET_ARCH="linux-amd64"
 else
   echo "ERROR: unknown platform specifier - $1. Only --arm64 or --amd64 is supported"
   exit 1
 fi
 
-docker build --progress=plain --platform "${PLATFORM}" -t "$3" -f "Dockerfile.$2" .
+dotnet publish ../../src/Playwright -o dist/ --arch $DOTNET_ARCH
+
+docker build --progress=plain --platform "${DOCKER_PLATFORM}" -t "$3" -f "Dockerfile.$2" .
