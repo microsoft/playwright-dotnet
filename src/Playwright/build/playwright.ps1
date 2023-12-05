7d3706349b53ef782639ce1877c96fcfff327ace
Max Schmitt <max@schmitt.mx>
2023-12-05 09:20:07 -0800

    fix: Microsoft.Playwright.dll is in use (#2785)

--- src/Playwright/build/playwright.ps1
+++ src/Playwright/build/playwright.ps1
@@ -1,5 +1,7 @@
 #!/usr/bin/env pwsh
 
-$PlaywrightFileName = Join-Path $PSScriptRoot "Microsoft.Playwright.dll"
-[Reflection.Assembly]::LoadFile($PlaywrightFileName) | Out-Null
+$Env:PLAYWRIGHT_DRIVER_SEARCH_PATH = $PSScriptRoot;
+$playwrightLibrary = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "Microsoft.Playwright.dll"))
+# We load the library via the memory to not keep the .dll file locked/opened.
+[Reflection.Assembly]::Load([System.IO.File]::ReadAllBytes($playwrightLibrary)) | Out-Null
 exit [Microsoft.Playwright.Program]::Main($args)
