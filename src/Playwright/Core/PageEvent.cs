519ba353f9643437ae8329c4b2e88268e6d813ec
Max Schmitt <max@schmitt.mx>
2023-12-09 18:38:24 -0800

    chore: do not expose PlaywrightEvent

--- src/Playwright/Core/PageEvent.cs
+++ src/Playwright/Core/PageEvent.cs
@@ -26,88 +26,37 @@ namespace Microsoft.Playwright.Core;
 
 internal static class PageEvent
 {
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Request"/>.
-    /// </summary>
     public static PlaywrightEvent<IRequest> Request { get; } = new() { Name = "Request" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.RequestFinished"/>.
-    /// </summary>
     public static PlaywrightEvent<IRequest> RequestFinished { get; } = new() { Name = "RequestFinished" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Crash"/>.
-    /// </summary>
     public static PlaywrightEvent<IPage> Crash { get; } = new() { Name = "Crash" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Close"/>.
-    /// </summary>
     public static PlaywrightEvent<IPage> Close { get; } = new() { Name = "Close" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Response"/>.
-    /// </summary>
     public static PlaywrightEvent<IResponse> Response { get; } = new() { Name = "Response" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Download"/>.
-    /// </summary>
     public static PlaywrightEvent<IDownload> Download { get; } = new() { Name = "Download" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Console"/>.
-    /// </summary>
     public static PlaywrightEvent<IConsoleMessage> Console { get; } = new() { Name = "Console" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Popup"/>.
-    /// </summary>
     public static PlaywrightEvent<IPage> Popup { get; } = new() { Name = "Popup" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.FrameNavigated"/>.
-    /// </summary>
     public static PlaywrightEvent<IFrame> FrameNavigated { get; } = new() { Name = "FrameNavigated" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.FrameDetached"/>.
-    /// </summary>
     public static PlaywrightEvent<IFrame> FrameDetached { get; } = new() { Name = "FrameDetached" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Worker"/>.
-    /// </summary>
     public static PlaywrightEvent<IWorker> Worker { get; } = new() { Name = "Worker" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Dialog"/>.
-    /// </summary>
     public static PlaywrightEvent<IDialog> Dialog { get; } = new() { Name = "Dialog" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.FileChooser"/>.
-    /// </summary>
     public static PlaywrightEvent<IFileChooser> FileChooser { get; } = new() { Name = "FileChooser" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.PageError"/>.
-    /// </summary>
     public static PlaywrightEvent<string> PageError { get; } = new() { Name = "PageError" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.Load"/>.
-    /// </summary>
     public static PlaywrightEvent<IPage> Load { get; } = new() { Name = "Load" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.DOMContentLoaded"/>.
-    /// </summary>
     public static PlaywrightEvent<IPage> DOMContentLoaded { get; } = new() { Name = "DOMContentLoaded" };
 
-    /// <summary>
-    /// <see cref="PlaywrightEvent{T}"/> representing a <see cref="IPage.WebSocket"/>.
-    /// </summary>
     public static PlaywrightEvent<IWebSocket> WebSocket { get; } = new() { Name = "WebSocket" };
 }
