84db8c55ac08c91056b7c4db9e65e6876b78bf97
Max Schmitt <max@schmitt.mx>
2024-11-27 12:55:46 +0000

    harden check

--- src/Playwright.TestAdapter/PlaywrightSettingsProvider.cs
+++ src/Playwright.TestAdapter/PlaywrightSettingsProvider.cs
@@ -41,7 +41,8 @@ public class PlaywrightSettingsProvider : ISettingsProvider
         {
             var browserFromEnv = Environment.GetEnvironmentVariable("BROWSER")?.ToLowerInvariant();
             // GitHub Codespaces sets the BROWSER environment variable, ignore it if its bogus.
-            if (!string.IsNullOrEmpty(browserFromEnv) && !browserFromEnv!.StartsWith("/vscode/"))
+            var ignoreValueFromEnv = Environment.GetEnvironmentVariable("CODESPACES") == "true" && browserFromEnv!.StartsWith("/vscode/");
+            if (!string.IsNullOrEmpty(browserFromEnv) && !ignoreValueFromEnv)
             {
                 ValidateBrowserName(browserFromEnv!, "'BROWSER' environment variable", "\nTry to remove 'BROWSER' environment variable for using default browser");
                 return browserFromEnv!;
