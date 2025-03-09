08d594bb1db00e53a750d568b67049037c637dc3
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2025-03-09 00:32:55 -0800

    fix: comma

--- src/tools/Playwright.Tooling/DriverDownloader.cs
+++ src/tools/Playwright.Tooling/DriverDownloader.cs
@@ -153,7 +153,7 @@ internal class DriverDownloader
 
         var cr_devtools_path = Path.Combine(chromium_path, "crDevTools.js");
 
-        await ReplaceInFileAsync(cr_devtools_path, "session.send('Runtime.enable')", "/*session.send('Runtime.enable'), */").ConfigureAwait(false);
+        await ReplaceInFileAsync(cr_devtools_path, "session.send('Runtime.enable'), ", "/*session.send('Runtime.enable'), */").ConfigureAwait(false);
 
         var cr_page_path = Path.Combine(chromium_path, "crPage.js");
         await ReplaceInFileAsync(cr_page_path, "this._client.send('Runtime.enable', {}),", "/*this._client.send('Runtime.enable', {}),*/").ConfigureAwait(false);
