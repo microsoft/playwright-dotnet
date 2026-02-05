3efaecb1f0952f4c994a054afa3cad4cf422230d
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-02-05 21:04:29 +0300

    Return original List type for loadStates

--- src/Playwright/Core/Frame.cs
+++ src/Playwright/Core/Frame.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Playwright.Core;
 
 internal class Frame : ChannelOwner, IFrame
 {
-    private readonly HashSet<WaitUntilState> _loadStates = new();
+    private readonly List<WaitUntilState> _loadStates = new();
     private readonly object _loadStatesLock = new();
     internal readonly List<Frame> _childFrames = new();
 
@@ -51,11 +51,7 @@ internal class Frame : ChannelOwner, IFrame
         Url = initializer.Url;
         Name = initializer.Name;
         ParentFrame = initializer.ParentFrame;
-
-        foreach (var state in initializer.LoadStates)
-        {
-            _loadStates.Add(state);
-        }
+        _loadStates = initializer.LoadStates;
     }
 
     /// <summary>
